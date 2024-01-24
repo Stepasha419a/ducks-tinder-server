5c575b0cf794183f53171161c63280d20de426f2
Stepan <Dark_Magic419a@mail.ru>
2024-01-24 23:15:34 +0300

    feature: rabbitmq services register implementation

--- libs/common/src/rabbitmq/rabbitmq.module.ts
+++ libs/common/src/rabbitmq/rabbitmq.module.ts
@@ -1,8 +1,33 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
 import { RabbitMQService } from './rabbitmq.service';
+import { Module as ModuleType } from '@nestjs/core/injector/module';
+import { ClientsModule, RmqOptions, Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   providers: [RabbitMQService],
   exports: [RabbitMQService],
 })
-export class RabbitMQModule {}
+export class RabbitMQModule {
+  static create({ name }: ModuleType): DynamicModule {
+    return {
+      module: RabbitMQModule,
+      imports: [
+        ClientsModule.registerAsync([
+          {
+            name,
+            useFactory: (configService: ConfigService): RmqOptions => ({
+              transport: Transport.RMQ,
+              options: {
+                urls: [configService.get<string>('RABBIT_MQ_URI')],
+                queue: configService.get<string>(`RABBIT_MQ_${name}_QUEUE`),
+              },
+            }),
+            inject: [ConfigService],
+          },
+        ]),
+      ],
+      exports: [ClientsModule],
+    };
+  }
+}
