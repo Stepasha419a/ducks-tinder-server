950d1e74bfd905f8530255dee44f676638b71ea1
Stepan <Dark_Magic419a@mail.ru>
2024-02-06 23:21:41 +0300

    test: patch-user user command unit tests refactoring

--- apps/user/src/application/command/patch-user/patch-user.command-handler.spec.ts
+++ apps/user/src/application/command/patch-user/patch-user.command-handler.spec.ts
@@ -1,132 +1,128 @@
 import { Test } from '@nestjs/testing';
-import { FilesModule } from 'files/files.module';
-import { PrismaModule } from '@app/common/database/database.module';
-import { PrismaService } from '@app/common/database/database.service';
-import { UsersPrismaMock } from 'apps/user/src/test/mocks';
-import { UserDto } from 'apps/user/src/legacy/dto';
 import { PatchUserCommandHandler } from './patch-user.command-handler';
 import { PatchUserCommand } from './patch-user.command';
-import { requestUserStub, userDtoStub } from 'apps/user/src/test/stubs';
-import { UPDATE_USER_DTO } from 'apps/user/src/test/values/users.const.dto';
-import { USER_ALREADY_EXISTS } from '@app/common/constants/error';
-import { UsersSelector } from 'apps/user/src/infrastructure/repository/user.selector';
+import { UserRepository } from 'apps/user/src/domain/repository';
+import { UserRepositoryMock } from 'apps/user/src/test/mock';
+import { UserAggregateStub, UserStub } from 'apps/user/src/test/stub';
+import { UserAggregate } from 'apps/user/src/domain';
+import { COMMON_ERROR } from '@app/common/shared/constant';
 
 describe('when patch is called', () => {
-  let prismaService: PrismaService;
+  let repository: UserRepository;
   let patchUserCommandHandler: PatchUserCommandHandler;
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
-      providers: [PatchUserCommandHandler],
-      imports: [FilesModule, PrismaModule],
-    })
-      .overrideProvider(PrismaService)
-      .useValue(UsersPrismaMock())
-      .compile();
+      providers: [
+        PatchUserCommandHandler,
+        { provide: UserRepository, useValue: UserRepositoryMock() },
+      ],
+    }).compile();
 
     patchUserCommandHandler = moduleRef.get<PatchUserCommandHandler>(
       PatchUserCommandHandler,
     );
-    prismaService = moduleRef.get<PrismaService>(PrismaService);
+    repository = moduleRef.get<UserRepository>(UserRepository);
   });
 
   describe('when it is called correctly', () => {
     beforeAll(() => {
-      prismaService.user.findUnique = jest.fn().mockResolvedValue(undefined);
-      prismaService.$transaction = jest.fn();
-      prismaService.user.update = jest.fn().mockResolvedValue({
-        ...userDtoStub(),
-        pairs: [userDtoStub().firstPair],
-      });
-      prismaService.user.count = jest.fn().mockResolvedValue(5);
+      repository.findOne = jest.fn().mockResolvedValue(UserAggregateStub());
+      repository.findOneByEmail = jest.fn().mockResolvedValue(undefined);
+      repository.save = jest.fn().mockResolvedValue(UserAggregateStub());
     });
 
-    let user: UserDto;
+    const dto = {
+      name: 'William',
+      email: 'email123123@gmail.com',
+    };
+
+    let response: UserAggregate;
 
     beforeEach(async () => {
       jest.clearAllMocks();
 
-      user = await patchUserCommandHandler.execute(
-        new PatchUserCommand(requestUserStub(), UPDATE_USER_DTO),
+      response = await patchUserCommandHandler.execute(
+        new PatchUserCommand(UserStub().id, dto),
       );
     });
 
-    it('should call user find unique', () => {
-      expect(prismaService.user.findUnique).toBeCalledTimes(1);
-      expect(prismaService.user.findUnique).toHaveBeenNthCalledWith(1, {
-        where: { email: UPDATE_USER_DTO.email },
-      });
+    it('should call repository findOne', () => {
+      expect(repository.findOne).toHaveBeenCalledTimes(1);
+      expect(repository.findOne).toHaveBeenCalledWith(UserStub().id);
     });
 
-    it('should call update user', () => {
-      expect(prismaService.user.update).toBeCalledTimes(1);
-      expect(prismaService.user.update).toBeCalledWith({
-        where: { id: requestUserStub().id },
-        data: { ...UPDATE_USER_DTO },
-        include: UsersSelector.selectUser(),
-      });
+    it('should call repository findOneByEmail', () => {
+      expect(repository.findOneByEmail).toHaveBeenCalledTimes(1);
+      expect(repository.findOneByEmail).toHaveBeenCalledWith(dto.email);
     });
 
-    it('should call user count', () => {
-      expect(prismaService.user.count).toBeCalledTimes(1);
-      expect(prismaService.user.count).toBeCalledWith({
-        where: { pairFor: { some: { id: requestUserStub().id } } },
-      });
+    it('should call repository save', () => {
+      expect(repository.save).toHaveBeenCalledTimes(1);
+
+      const calledWithArg = (repository.save as jest.Mock).mock.calls[0][0];
+      expect(JSON.parse(JSON.stringify(calledWithArg))).toEqual(
+        JSON.parse(JSON.stringify({ ...UserAggregateStub(), ...dto })),
+      );
     });
 
-    it('should return a user', () => {
-      expect(user).toEqual(userDtoStub());
+    it('should return a user aggregate', () => {
+      expect(JSON.parse(JSON.stringify(response))).toEqual(
+        JSON.parse(JSON.stringify(UserAggregateStub())),
+      );
     });
   });
 
   describe('when there is already used email address', () => {
     beforeAll(() => {
-      prismaService.user.findUnique = jest.fn().mockResolvedValue({
-        pairs: [userDtoStub().firstPair],
-      });
-      prismaService.user.update = jest.fn().mockResolvedValue({
-        ...userDtoStub(),
-        pairs: [userDtoStub().firstPair],
-      });
-      prismaService.user.count = jest.fn().mockResolvedValue(5);
+      repository.findOne = jest.fn().mockResolvedValue(UserAggregateStub());
+      repository.findOneByEmail = jest
+        .fn()
+        .mockResolvedValue(UserAggregateStub());
+      repository.save = jest.fn().mockResolvedValue(UserAggregateStub());
     });
 
-    let user: UserDto;
+    const dto = {
+      name: 'William',
+      email: 'email123123@gmail.com',
+    };
+
+    let response: UserAggregate;
     let error;
 
     beforeEach(async () => {
       jest.clearAllMocks();
+
       try {
-        user = await patchUserCommandHandler.execute(
-          new PatchUserCommand(requestUserStub(), UPDATE_USER_DTO),
+        response = await patchUserCommandHandler.execute(
+          new PatchUserCommand(UserStub().id, dto),
         );
       } catch (responseError) {
         error = responseError;
       }
     });
 
-    it('should call user find unique', () => {
-      expect(prismaService.user.findUnique).toBeCalledTimes(1);
-      expect(prismaService.user.findUnique).toHaveBeenNthCalledWith(1, {
-        where: { email: UPDATE_USER_DTO.email },
-      });
+    it('should call repository findOne', () => {
+      expect(repository.findOne).toHaveBeenCalledTimes(1);
+      expect(repository.findOne).toHaveBeenCalledWith(UserStub().id);
     });
 
-    it('should not call update user', () => {
-      expect(prismaService.user.update).not.toBeCalled();
+    it('should call repository findOneByEmail', () => {
+      expect(repository.findOneByEmail).toHaveBeenCalledTimes(1);
+      expect(repository.findOneByEmail).toHaveBeenCalledWith(dto.email);
     });
 
-    it('should not call user count', () => {
-      expect(prismaService.user.count).not.toBeCalled();
+    it('should not call repository save', () => {
+      expect(repository.save).not.toHaveBeenCalled();
     });
 
     it('should throw an error', () => {
       expect(error.status).toEqual(403);
-      expect(error.message).toEqual(USER_ALREADY_EXISTS);
+      expect(error.message).toEqual(COMMON_ERROR.USER_ALREADY_EXISTS);
     });
 
     it('should return undefined', () => {
-      expect(user).toEqual(undefined);
+      expect(response).toEqual(undefined);
     });
   });
 });
