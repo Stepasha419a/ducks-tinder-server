f319e014214bdb3f2739d0467377756dcde9ffe5
Stepan <Dark_Magic419a@mail.ru>
2024-05-29 23:45:08 +0300

    feature: auth-service auth-user domain entity

---
+++ auth-service/internal/domain/entity/auth_user.go
@@ -0,0 +1,33 @@
+package domain
+
+import (
+	crypto_service "auth-service/internal/domain/service/crypto"
+	"os"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type AuthUser struct {
+	Id           string
+	Email        string
+	Password     string
+	RefreshToken string
+
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+func NewAuthUser(email string, password string, refreshToken string) *AuthUser {
+	hashedPassword := crypto_service.Hash(password, []byte(os.Getenv("PASSWORD_SALT")))
+
+	return &AuthUser{
+		Id:           uuid.New().String(),
+		Email:        email,
+		Password:     hashedPassword,
+		RefreshToken: refreshToken,
+
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+}
