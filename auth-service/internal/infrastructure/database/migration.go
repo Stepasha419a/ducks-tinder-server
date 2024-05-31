0fa99a548cf778123b52c843a51c607373ffaeed
Stepan <Dark_Magic419a@mail.ru>
2024-05-31 23:55:46 +0300

    feature: auth-service database migrations

---
+++ auth-service/internal/infrastructure/database/migration.go
@@ -0,0 +1,74 @@
+package database
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"os"
+
+	log "log/slog"
+
+	"github.com/joho/godotenv"
+)
+
+func MigrateDB(pool *Postgres) {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		panic("Error loading .env file")
+	}
+
+	ctx := context.TODO()
+
+	submitted := submitMigration()
+
+	if !submitted {
+		log.Info("migration - canceled")
+		return
+	}
+
+	log.Info("migration - start")
+
+	err = initMigration(ctx, pool)
+	if err != nil {
+		log.Info("migration - failed")
+		panic(err)
+	}
+
+	log.Info("migration - successful")
+}
+
+func submitMigration() bool {
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print("migration - submit (y/n): ")
+	submitted := false
+	for scanner.Scan() {
+		text := scanner.Text()
+		if text != "" {
+			if text == "y" {
+				submitted = true
+			}
+
+			return submitted
+		}
+	}
+	return submitted
+}
+
+func initMigration(ctx context.Context, pool *Postgres) error {
+	log.Info("migration - init tables migration")
+
+	_, err := pool.Db.Exec(ctx, `CREATE TABLE IF NOT EXISTS auth_users (
+		id UUID PRIMARY KEY,
+		email VARCHAR(100) NOT NULL UNIQUE,
+		password VARCHAR(255) NOT NULL,
+		refreshToken text,
+		createdAt timestamp(3) without time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
+		updatedAt timestamp(3) without time zone NOT NULL DEFAULT CURRENT_TIMESTAMP
+	)`)
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
