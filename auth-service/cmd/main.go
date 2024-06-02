f2465285cb1e88d4f83198d6f27d542ddb002215
Stepan <Dark_Magic419a@mail.ru>
2024-06-02 11:33:31 +0300

    feature: auth-service http handling

--- auth-service/cmd/main.go
+++ auth-service/cmd/main.go
@@ -1,6 +1,14 @@
 package main
 
 import (
+	"auth-service/internal/application/facade"
+	"auth-service/internal/infrastructure/database"
+	"auth-service/internal/infrastructure/repository"
+	"auth-service/internal/interface/http/controller"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
@@ -10,4 +18,17 @@ func main() {
 	if err != nil {
 		panic("Error loading .env file")
 	}
+
+	pgPool := database.NewPostgresInstance()
+
+	authUserRepository := repository.NewAuthUserRepository(pgPool)
+
+	authFacade := facade.NewAuthFacade(authUserRepository)
+
+	e := gin.Default()
+
+	controller.NewAuthController(e, authFacade)
+
+	PORT := os.Getenv("PORT")
+	http.ListenAndServe("127.0.0.1:"+PORT, e)
 }
