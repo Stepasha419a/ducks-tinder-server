07f8eb130f6a21a9193bf7992214ed77df7d29a0
Stepan <Dark_Magic419a@mail.ru>
2024-06-10 15:39:26 +0300

    feature: auth-service config-service

--- auth-service/cmd/main.go
+++ auth-service/cmd/main.go
@@ -6,20 +6,16 @@ import (
 	"auth-service/internal/infrastructure/adapter"
 	"auth-service/internal/infrastructure/database"
 	"auth-service/internal/infrastructure/repository"
+	config_service "auth-service/internal/infrastructure/service/config"
 	auth_controller "auth-service/internal/interface/http/controller/auth"
 	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load(".env")
-
-	if err != nil {
-		panic("Error loading .env file")
-	}
+	config_service.RequireEnv()
 
 	db := database.NewPostgresInstance()
 	transactionService := database.NewTransactionService(db.Pool)
