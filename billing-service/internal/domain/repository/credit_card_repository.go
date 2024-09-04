634d76c96386f86bbd851949ba56925112d63e17
Stepan <Dark_Magic419a@mail.ru>
2024-09-04 12:23:00 +0300

    feature: billing-service credit-card-repository useless aggregation method

--- billing-service/internal/domain/repository/credit_card_repository.go
+++ billing-service/internal/domain/repository/credit_card_repository.go
@@ -13,6 +13,5 @@ type CreditCardRepository interface {
 	Find(ctx context.Context, id string, tx pgx.Tx) (*domain.CreditCard, error)
 	FindByUserId(ctx context.Context, userId string, tx pgx.Tx) (*domain.CreditCard, error)
 	FindPurchase(ctx context.Context, id string, tx pgx.Tx) (*domain.Purchase, error)
-	FindCreditCardPurchasesAmountSum(ctx context.Context, creditCardId string, tx pgx.Tx) (*int64, error)
 	Delete(ctx context.Context, id string, tx pgx.Tx) error
 }
