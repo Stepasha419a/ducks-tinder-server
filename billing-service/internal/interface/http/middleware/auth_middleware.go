527f5f869a10ae7365f425c693353f85a0693878
Stepan <Dark_Magic419a@mail.ru>
2024-09-10 21:15:53 +0300

    feature: billing-service auth-middleware

---
+++ billing-service/internal/interface/http/middleware/auth_middleware.go
@@ -0,0 +1,32 @@
+package middleware
+
+import (
+	jwt_service "billing-service/internal/domain/service/jwt"
+	response_service "billing-service/internal/domain/service/response"
+	"net/http"
+
+	"github.com/gofiber/fiber/v3"
+)
+
+func authMiddleware(ctx fiber.Ctx, jwtService *jwt_service.JwtService) error {
+	authorization := ctx.Get(fiber.HeaderAuthorization)
+
+	if authorization == "" || len(authorization) < 7 || authorization[:6] != "Bearer" {
+		return ctx.Status(http.StatusUnauthorized).JSON(response_service.UnauthorizedResponse)
+	}
+
+	isValid, payload := jwtService.ValidateAccessToken(authorization[7:])
+	if !isValid {
+		return ctx.Status(http.StatusUnauthorized).JSON(response_service.UnauthorizedResponse)
+	}
+
+	ctx.Locals("userId", payload.UserId)
+
+	return ctx.Next()
+}
+
+func newAuthMiddleware(jwtService *jwt_service.JwtService) func(ctx fiber.Ctx) error {
+	return func(ctx fiber.Ctx) error {
+		return authMiddleware(ctx, jwtService)
+	}
+}
