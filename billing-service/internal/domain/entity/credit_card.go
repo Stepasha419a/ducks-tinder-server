b46789ea88980f312b2dcf886f8ddcc6f78db69c
Stepan <Dark_Magic419a@mail.ru>
2024-09-11 18:34:19 +0300

    feature: billing-service new-credit-card

--- billing-service/internal/domain/entity/credit_card.go
+++ billing-service/internal/domain/entity/credit_card.go
@@ -1,6 +1,11 @@
 package entity
 
-import "time"
+import (
+	time_service "billing-service/internal/domain/service/time"
+	"time"
+
+	"github.com/google/uuid"
+)
 
 type CreditCard struct {
 	Id        string
@@ -12,4 +17,23 @@ type CreditCard struct {
 
 	CreatedAt time.Time
 	UpdatedAt time.Time
+}
+
+func NewCreditCard(userId string, pan string, holder string, cvc string, expiresAt string) (*CreditCard, error) {
+	expiresAtTime, err := time_service.ParseISOString(expiresAt)
+	if err != nil {
+		return nil, err
+	}
+
+	return &CreditCard{
+		Id:        uuid.New().String(),
+		UserId:    userId,
+		Pan:       pan,
+		Holder:    holder,
+		Cvc:       cvc,
+		ExpiresAt: expiresAtTime,
+
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}, nil
 }
