e887ce80c024fc33173c44af5cfff4b09726ac0e
Stepan <Dark_Magic419a@mail.ru>
2024-09-12 11:35:45 +0300

    feature: billing-service service-context, service-no-return

--- billing-service/internal/application/service/billing_service.go
+++ billing-service/internal/application/service/billing_service.go
@@ -1,13 +1,12 @@
 package service
 
 import (
-	"billing-service/internal/application/mapper"
 	get_user_credit_card "billing-service/internal/application/query/get-user-credit-card"
-	"context"
+	service_context "billing-service/internal/application/service/context"
 )
 
 type (
 	BillingService interface {
-		GetUserCreditCard(ctx context.Context, query *get_user_credit_card.GetUserCreditCardQuery) (*mapper.CreditCardResponse, error)
+		GetUserCreditCard(ctx service_context.ServiceContext, query *get_user_credit_card.GetUserCreditCardQuery) error
 	}
 )
