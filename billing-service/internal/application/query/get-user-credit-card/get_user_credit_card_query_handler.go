c203bf41a717fd0f37d629b29d6ee18afb44fa26
Stepan <Dark_Magic419a@mail.ru>
2024-09-13 19:47:33 +0300

    feature: billing-service get-user-credit-card-query-handler service-context

--- billing-service/internal/application/query/get-user-credit-card/get_user_credit_card_query_handler.go
+++ billing-service/internal/application/query/get-user-credit-card/get_user_credit_card_query_handler.go
@@ -1,16 +1,17 @@
 package get_user_credit_card
 
 import (
+	response_service "billing-service/internal/application/common/response"
 	"billing-service/internal/application/mapper"
+	service_context "billing-service/internal/application/service/context"
 	"billing-service/internal/domain/repository"
-	"context"
 	"net/http"
 )
 
-func GetUserCreditCardQueryHandler(ctx context.Context, query *GetUserCreditCardQuery, responseError func(status int, message string), creditCardRepository repository.CreditCardRepository) (*mapper.CreditCardResponse, error) {
-	creditCard, err := creditCardRepository.FindByUserId(ctx, query.UserId, nil)
+func GetUserCreditCardQueryHandler(ctx service_context.ServiceContext, query *GetUserCreditCardQuery, creditCardRepository repository.CreditCardRepository) error {
+	creditCard, err := creditCardRepository.FindByUserId(ctx.Context(), query.UserId, nil)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	if creditCard == nil {
