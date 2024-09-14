b13bebcc8aa80a58b13e47ccc2672e8259d5c7e9
Stepan <Dark_Magic419a@mail.ru>
2024-09-14 13:57:30 +0300

    feature: billing-service response-service map-response

--- billing-service/internal/application/common/response/response.go
+++ billing-service/internal/application/common/response/response.go
@@ -1,8 +1,17 @@
 package response_service
 
+import "strconv"
+
 var (
 	InternalServerErrorResponse = map[string]string{"status": "500", "message": "Internal server error"}
 	BadRequest                  = map[string]string{"status": "400", "message": "BadRequest"}
 	UnauthorizedResponse        = map[string]string{"status": "401", "message": "Unauthorized"}
 	NotFound                    = map[string]string{"status": "404", "message": "Not found"}
 )
+
+func MapResponse(status int, message string) map[string]interface{} {
+	return map[string]interface{}{
+		"status":  strconv.Itoa(status),
+		"message": message,
+	}
+}
