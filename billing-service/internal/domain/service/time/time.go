cbf3f5c5e9bd2694ceb1910a44d5ebb6baed9569
Stepan <Dark_Magic419a@mail.ru>
2024-09-15 20:18:16 +0300

    feature: billing-service time-service parse-date-month-string

--- billing-service/internal/domain/service/time/time.go
+++ billing-service/internal/domain/service/time/time.go
@@ -5,3 +5,7 @@ import "time"
 func ParseISOString(timeString string) (time.Time, error) {
 	return time.Parse("2006-01-02T15:04:05-0700", timeString)
 }
+
+func ParseDateMonthString(timeString string) (time.Time, error) {
+	return time.Parse("2006-01", timeString)
+}
