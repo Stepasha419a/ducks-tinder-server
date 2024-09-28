97759330d6f61c8a853a8962513414f7d91a62d3
Stepan <Dark_Magic419a@mail.ru>
2024-09-28 09:22:48 +0300

    feature: billing-service credit-card grpc-response

--- billing-service/internal/interface/grpc/response/response.go
+++ billing-service/internal/interface/grpc/response/response.go
@@ -21,3 +21,25 @@ func (r *PurchaseResponse) ToPurchaseGrpcResponse() *gen.Purchase {
 		CreatedAt:    createdAt,
 	}
 }
+
+type CreditCardResponse struct {
+	*mapper.CreditCardResponse
+}
+
+func (r *CreditCardResponse) ToCreditCardGrpcResponse() *gen.CreditCard {
+	expiresAt := timestamppb.New(r.ExpiresAt)
+	createdAt := timestamppb.New(r.CreatedAt)
+	updatedAt := timestamppb.New(r.UpdatedAt)
+
+	return &gen.CreditCard{
+		Id:     r.Id,
+		UserId: r.UserId,
+		Pan:    r.Pan,
+		Holder: r.Holder,
+		Cvc:    r.Cvc,
+
+		ExpiresAt: expiresAt,
+		CreatedAt: createdAt,
+		UpdatedAt: updatedAt,
+	}
+}
