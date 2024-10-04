9cf90446bac09b30bea7c117f57478c87acd7d04
Stepan <Dark_Magic419a@mail.ru>
2024-10-04 08:22:06 +0300

    feature: billing-service billing-service withdraw-user-credit-card

--- billing-service/internal/application/service/billing_service.go
+++ billing-service/internal/application/service/billing_service.go
@@ -1,6 +1,8 @@
 package service
 
 import (
+	"billing-service/internal/application/command/add_user_credit_card"
+	"billing-service/internal/application/command/withdraw_user_credit_card"
 	"billing-service/internal/application/mapper"
 	get_user_credit_card "billing-service/internal/application/query/get-user-credit-card"
 	service_context "billing-service/internal/application/service/context"
@@ -10,5 +12,6 @@ type (
 	BillingService interface {
 		GetUserCreditCard(ctx service_context.ServiceContext[*mapper.CreditCardResponse], query *get_user_credit_card.GetUserCreditCardQuery) error
 		AddUserCreditCard(ctx service_context.ServiceContext[*mapper.CreditCardResponse], command *add_user_credit_card.AddUserCreditCardCommand) error
+		WithdrawUserCreditCard(ctx service_context.ServiceContext[*mapper.PurchaseResponse], command *withdraw_user_credit_card.WithdrawUserCreditCardCommand) error
 	}
 )
