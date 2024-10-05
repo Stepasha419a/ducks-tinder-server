6107b5a4c328df0b21ad8c56210cdd8c36d1cd13
Stepan <Dark_Magic419a@mail.ru>
2024-10-05 17:22:01 +0300

    feature: billing-service billing-application-service delete-user-credit-card

--- billing-service/internal/application/service/billing_service.go
+++ billing-service/internal/application/service/billing_service.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"billing-service/internal/application/command/add_user_credit_card"
+	"billing-service/internal/application/command/delete_user_credit_card"
 	"billing-service/internal/application/command/withdraw_user_credit_card"
 	"billing-service/internal/application/mapper"
 	get_user_credit_card "billing-service/internal/application/query/get-user-credit-card"
@@ -13,5 +14,6 @@ type (
 		GetUserCreditCard(ctx service_context.ServiceContext[*mapper.CreditCardResponse], query *get_user_credit_card.GetUserCreditCardQuery) error
 		AddUserCreditCard(ctx service_context.ServiceContext[*mapper.CreditCardResponse], command *add_user_credit_card.AddUserCreditCardCommand) error
 		WithdrawUserCreditCard(ctx service_context.ServiceContext[*mapper.PurchaseResponse], command *withdraw_user_credit_card.WithdrawUserCreditCardCommand) error
+		DeleteUserCreditCard(ctx service_context.ServiceContext[*mapper.CreditCardResponse], command *delete_user_credit_card.DeleteUserCreditCardCommand) error
 	}
 )
