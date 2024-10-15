a743d33a55c27c9eee422e822b5bd7f279b49c1c
Stepan <Dark_Magic419a@mail.ru>
2024-10-15 08:42:08 +0300

    feature: cookiecutter service-context-impl

---
+++ cookiecutter/{{cookiecutter.project_slug}}/internal/interface/http/fiber/context/service_context_impl.go
@@ -0,0 +1,60 @@
+package fiber_impl_context
+
+import (
+	"context"
+	"net/http"
+	"strconv"
+
+	service_context "{{cookiecutter.module_name}}/internal/application/service/context"
+
+	"github.com/gofiber/fiber/v3"
+)
+
+var (
+	InternalServerErrorResponse = map[string]string{"status": "500", "message": "Internal server error"}
+	BadRequest                  = map[string]string{"status": "400", "message": "BadRequest"}
+	UnauthorizedResponse        = map[string]string{"status": "401", "message": "Unauthorized"}
+	NotFound                    = map[string]string{"status": "404", "message": "Not found"}
+)
+
+type ServiceContextImpl[R any] struct {
+	ctx          fiber.Ctx
+	ResponseData R
+}
+
+func NewServiceContext[R any](ctx fiber.Ctx) service_context.ServiceContext[R] {
+	return &ServiceContextImpl[R]{ctx: ctx}
+}
+
+func (s *ServiceContextImpl[R]) Context() context.Context {
+	return s.ctx.Context()
+}
+
+func (s *ServiceContextImpl[R]) Response(status int, body R) error {
+	s.ResponseData = body
+
+	return s.ctx.Status(status).JSON(body)
+}
+
+func (s *ServiceContextImpl[R]) BadRequest() error {
+	return s.ctx.Status(http.StatusBadRequest).JSON(BadRequest)
+}
+
+func (s *ServiceContextImpl[R]) Unauthorized() error {
+	return s.ctx.Status(http.StatusUnauthorized).JSON(UnauthorizedResponse)
+}
+
+func (s *ServiceContextImpl[R]) NotFound() error {
+	return s.ctx.Status(http.StatusNotFound).JSON(NotFound)
+}
+
+func (s *ServiceContextImpl[R]) InternalServerError() error {
+	return s.ctx.Status(http.StatusInternalServerError).JSON(InternalServerErrorResponse)
+}
+
+func MapResponse(status int, message string) map[string]interface{} {
+	return map[string]interface{}{
+		"status":  strconv.Itoa(status),
+		"message": message,
+	}
+}
