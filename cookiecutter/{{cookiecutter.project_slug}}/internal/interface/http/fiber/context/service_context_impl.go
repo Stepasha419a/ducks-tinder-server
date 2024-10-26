ab132a65b70123eb9902abe4e9a23c4836962843
Stepan <Dark_Magic419a@mail.ru>
2024-10-26 19:36:24 +0300

    refactor: cookiecutter fiber-context-impl bad-request message

--- cookiecutter/{{cookiecutter.project_slug}}/internal/interface/http/fiber/context/service_context_impl.go
+++ cookiecutter/{{cookiecutter.project_slug}}/internal/interface/http/fiber/context/service_context_impl.go
@@ -12,7 +12,7 @@ import (
 
 var (
 	InternalServerErrorResponse = map[string]string{"status": "500", "message": "Internal server error"}
-	BadRequest                  = map[string]string{"status": "400", "message": "BadRequest"}
+	BadRequest                  = map[string]string{"status": "400", "message": "Bad Request"}
 	UnauthorizedResponse        = map[string]string{"status": "401", "message": "Unauthorized"}
 	NotFound                    = map[string]string{"status": "404", "message": "Not found"}
 )
