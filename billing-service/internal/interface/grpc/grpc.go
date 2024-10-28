504738c1ca09b7960f75db47ed74d5842e7d946e
Stepan <Dark_Magic419a@mail.ru>
2024-10-28 08:48:23 +0300

    feature: billing-service grpc-server ssl

--- billing-service/internal/interface/grpc/grpc.go
+++ billing-service/internal/interface/grpc/grpc.go
@@ -4,22 +4,36 @@ import (
 	config_service "billing-service/internal/domain/service/config"
 	grpc_interceptor "billing-service/internal/interface/grpc/interceptor"
 	"billing-service/proto/gen"
+	"crypto/tls"
 	"fmt"
 	"log"
 	"net"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
-func NewGrpc(billingServer gen.BillingServiceServer, interceptor *grpc_interceptor.GrpcInterceptor) (*grpc.Server, func()) {
-	server := grpc.NewServer(grpc.ChainUnaryInterceptor(interceptor.RecoveryUnaryInterceptor, interceptor.AuthUnaryInterceptor))
+func NewGrpc(billingServer gen.BillingServiceServer, interceptor *grpc_interceptor.GrpcInterceptor) (*grpc.Server, func(), error) {
+	// TODO: ssl
+	cert, err := tls.LoadX509KeyPair("cert/server.crt", "cert/server.key")
+	if err != nil {
+		return nil, nil, err
+	}
+	creds := credentials.NewServerTLSFromCert(&cert)
+
+	opts := []grpc.ServerOption{
+		grpc.ChainUnaryInterceptor(interceptor.RecoveryUnaryInterceptor, interceptor.AuthUnaryInterceptor),
+		grpc.Creds(creds),
+	}
+
+	server := grpc.NewServer(opts...)
 
 	gen.RegisterBillingServiceServer(server, billingServer)
 
 	return server, func() {
 		log.Println("close grpc server")
 		server.GracefulStop()
-	}
+	}, nil
 }
 
 func InitGrpcListener(server *grpc.Server, configService config_service.ConfigService) error {
