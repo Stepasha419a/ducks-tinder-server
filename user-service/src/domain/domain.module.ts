b496d7b944c800af2d9194a1b87df3309a86cec7
Stepan <Dark_Magic419a@mail.ru>
2024-10-31 08:40:21 +0300

    feature: user-service date-service domain-module

--- user-service/src/domain/domain.module.ts
+++ user-service/src/domain/domain.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { JwtService } from './service/jwt';
 import { JwtModule as NestJwtModule } from '@nestjs/jwt';
+import { DateService } from './service/date/date.service';
 
 @Module({
-  providers: [JwtService],
-  exports: [JwtService],
+  providers: [JwtService, DateService],
+  exports: [JwtService, DateService],
   imports: [NestJwtModule],
 })
 export class DomainModule {}
