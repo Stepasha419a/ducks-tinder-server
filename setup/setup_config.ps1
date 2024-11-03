bf7e599a125ed0e00c0d157c051f98dba3bec429
Stepan <Dark_Magic419a@mail.ru>
2024-11-03 14:14:27 +0300

    feature: setup setup-config replace configs with env-secrets

--- setup/setup_config.ps1
+++ setup/setup_config.ps1
@@ -1,7 +1,51 @@
 $golangServices = ("auth-service", "billing-service", "file-service", "subscription-service")
 $typescriptServices = ("user-service", "chat-service")
 
-$localVarsReplacement = ("postgresql://prisma:prisma@postgres:5432/", "postgresql://prisma:prisma@localhost:5433/"), ("host.docker.internal", "localhost"), ("amqp://rabbitmq:5672", "amqp://localhost:5672")
+$yamlConfigServices = ("database-seeder")
+$onlyDevConfigServices = ("database-seeder")
+
+$localVarsReplacement = ("postgresql://prisma:prisma@postgres:5432/", "postgresql://prisma:prisma@127.0.0.1:5433/"), ("host.docker.internal", "127.0.0.1"), ("amqp://rabbitmq:5672", "amqp://127.0.0.1:5672")
+
+$secretsReplacementEnvFilePath = "../.env.secrets"
+
+function GetSecretsReplacementEnvVariables {
+    $envVariables = @{}
+
+    if (-Not(Test-Path $secretsReplacementEnvFilePath)) {
+        return $envVariables
+    }
+
+    foreach ($line in Get-Content -Path $secretsReplacementEnvFilePath) {
+        if ($line -match "^\s*#") { continue }
+        if ($line -match "^\s*$") { continue }
+
+        $key, $value = $line -split "=", 2
+        $envVariables[$key.Trim()] = $value.Trim()
+    }
+
+    return $envVariables
+}
+
+$envVariables = GetSecretsReplacementEnvVariables
+
+function SetSecretsRelacement {
+    param (
+        $configPath
+    )
+
+    if (-Not(Test-Path $secretsReplacementEnvFilePath)) {
+        return
+    }
+
+    $content = Get-Content -Path $configPath
+    foreach ($key in $envVariables.Keys) {
+        $value = $envVariables[$key]
+
+        $content = $content.Replace($key, $value)
+    }
+
+    Set-Content $configPath $content
+}
 
 function SetLocalUrls {
     param (
@@ -16,8 +60,12 @@ function SetLocalUrls {
     Set-Content $configPath $content
 }
 
-foreach ($service in $golangServices) {
-    $servicePath = "../$service"
+
+
+function SetUpYamlConfigService {
+    param (
+        $servicePath
+    )
 
     $configPath = "$servicePath/config"
 
@@ -31,9 +79,15 @@ foreach ($service in $golangServices) {
 
         Copy-Item "$modeConfigPath/.env.example" "$modeConfigPath/.env"
 
-        Copy-Item "$configPath/example.yaml" $devConfigPath
-        Copy-Item "$configPath/example.yaml" $devDockerConfigPath
+        if (-Not($onlyDevConfigServices.Contains($service))) {
+            Copy-Item "$configPath/example.yaml" $devDockerConfigPath
+            
+            SetSecretsRelacement($devDockerConfigPath)
+        }
 
+        Copy-Item "$configPath/example.yaml" $devConfigPath
+
+        SetSecretsRelacement($devConfigPath)
         SetLocalUrls($devConfigPath)
     }
     else {
@@ -41,8 +95,23 @@ foreach ($service in $golangServices) {
     }
 }
 
+foreach ($service in $golangServices) {
+    $servicePath = "../$service"
+
+    SetUpYamlConfigService($servicePath)
+}
+
+foreach ($service in $yamlConfigServices) {
+    $servicePath = "../$service"
+
+    SetUpYamlConfigService($servicePath)
+}
+
 foreach ($service in $typescriptServices) {
     $servicePath = "../$service"
+
+    $devConfigPath = "$servicePath/.env.dev"
+    $devDockerConfigPath = "$servicePath/.env.dev-docker"
 
     if (Test-Path "$servicePath/.env.example") {
         Write-Host "setting up configs for $service..."
@@ -50,6 +119,8 @@ foreach ($service in $typescriptServices) {
         Copy-Item "$servicePath/.env.example" "$servicePath/.env.dev"
         Copy-Item "$servicePath/.env.example" "$servicePath/.env.dev-docker"
 
+        SetSecretsRelacement($devConfigPath)
+        SetSecretsRelacement($devDockerConfigPath)
         SetLocalUrls("$servicePath/.env.dev")
     }
     else {
