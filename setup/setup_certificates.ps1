4a0904e6f30134918ca68c8093f32ed832a58861
Stepan <Dark_Magic419a@mail.ru>
2024-11-04 16:35:48 +0300

    feature: setup setup-certificates

---
+++ setup/setup_certificates.ps1
@@ -0,0 +1,14 @@
+$services = ("user-service", "chat-service", "auth-service", "billing-service", "file-service", "subscription-service")
+
+$certExamplePath = "./cert-example"
+
+foreach ($service in $services) {
+    $servicePath = "../$service"
+    $serviceCertPath = $servicePath + "/cert"
+    $serviceCertExamplePath = $servicePath + "/cert-example"
+
+    if (-Not (Test-Path -Path $serviceCertPath)) {
+        Copy-item -Force -Recurse -Verbose $certExamplePath -Destination $servicePath
+        Rename-Item -Path $serviceCertExamplePath -NewName "cert"
+    }
+}
