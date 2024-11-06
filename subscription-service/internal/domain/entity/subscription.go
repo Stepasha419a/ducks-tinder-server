25e5aa0e7fa210bb5b239da17c40108f9ea66cc6
Stepan <Dark_Magic419a@mail.ru>
2024-11-06 08:48:13 +0300

    feature: subscription-service new-subscription-entity

--- subscription-service/internal/domain/entity/subscription.go
+++ subscription-service/internal/domain/entity/subscription.go
@@ -13,3 +13,13 @@ type Subscription struct {
 	CreatedAt time.Time
 }
 
+func NewSubscription(userId string, subscription string, login string) *Subscription {
+	return &Subscription{
+		UserId:       userId,
+		Subscription: subscription,
+		Login:        login,
+
+		ExpiresAt: time.Now().Add(time.Hour * 24 * 31),
+		CreatedAt: time.Now(),
+	}
+}
