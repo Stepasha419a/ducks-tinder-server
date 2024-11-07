c050fc8641f8b3be63546ab1abb22da5b86a8121
Stepan <Dark_Magic419a@mail.ru>
2024-11-07 09:01:08 +0300

    feature: subscription-service billing-service

---
+++ subscription-service/internal/domain/service/billing/billing.go
@@ -0,0 +1,24 @@
+package billing_service
+
+import (
+	"context"
+)
+
+type (
+	BillingService interface {
+		WithdrawUserCreditCard(ctx context.Context, request *WithdrawUserCreditCardRequest) (*Purchase, error)
+	}
+
+	WithdrawUserCreditCardRequest struct {
+		UserId       string
+		CreditCardId string
+		Amount       int64
+	}
+
+	Purchase struct {
+		Id           string
+		CreditCardId string
+		Amount       int64
+		CreatedAt    int64
+	}
+)
