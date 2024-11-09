bc79a504aa2d7eae2e5b9ebe628c1abebed8569d
Stepan <Dark_Magic419a@mail.ru>
2024-11-09 11:34:18 +0300

    feature: file-service grpc-ssl

--- file-service/internal/service/grpc/grpc.go
+++ file-service/internal/service/grpc/grpc.go
@@ -1,17 +1,35 @@
 package grpc_service
 
 import (
+	"crypto/tls"
 	"fmt"
 	config_service "go-file-server/internal/service/config"
 	"go-file-server/proto/gen"
 	"log"
 	"net"
+	"path"
 
 	"google.golang.org/grpc"
-)
+	"google.golang.org/grpc/credentials"
 
 func Init() {
-	server := grpc.NewServer(grpc.ChainUnaryInterceptor(RecoveryUnaryInterceptor, AuthUnaryInterceptor))
+	// TODO: ssl
+	certPath := path.Join("cert", config_service.GetConfig().Mode, "certificate.pem")
+	privateKeyPath := path.Join("cert", config_service.GetConfig().Mode, "private-key.pem")
+
+	cert, err := tls.LoadX509KeyPair(certPath, privateKeyPath)
+	if err != nil {
+		panic(err)
+	}
+	creds := credentials.NewServerTLSFromCert(&cert)
+
+	opts := []grpc.ServerOption{
+		grpc.ChainUnaryInterceptor(RecoveryUnaryInterceptor, AuthUnaryInterceptor),
+		grpc.Creds(creds),
+
+	}
+
+	server := grpc.NewServer(opts...)
 
 	fileServer := FileServiceServerImpl{}
 
