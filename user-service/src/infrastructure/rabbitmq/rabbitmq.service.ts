b7ef7cf91e2d9434757e53fcc0c73b88acb8c2c3
Stepan <Dark_Magic419a@mail.ru>
2024-11-11 08:57:45 +0300

    feature: user-service rabbitmq-service tls

--- user-service/src/infrastructure/rabbitmq/rabbitmq.service.ts
+++ user-service/src/infrastructure/rabbitmq/rabbitmq.service.ts
@@ -1,12 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { RmqOptions, Transport } from '@nestjs/microservices';
+import * as fs from 'fs';
+import * as path from 'path';
 
 @Injectable()
 export class RabbitMQService {
   constructor(private readonly configService: ConfigService) {}
 
   getOptions(queue: string, noAck = false): RmqOptions {
+    const mode = this.configService.get<string>('NODE_ENV');
+    const certPath = path.join('cert', mode, 'certificate.pem');
+    const rootCertPath = path.join('cert', mode, 'ca.crt');
+    const keyPath = path.join('cert', mode, 'private-key.pem');
+
     return {
       transport: Transport.RMQ,
       options: {
@@ -14,6 +21,13 @@ export class RabbitMQService {
         queue: this.configService.get<string>(`RABBIT_MQ_${queue}_QUEUE`),
         noAck,
         persistent: true,
+        socketOptions: {
+          connectionOptions: {
+            cert: fs.readFileSync(certPath),
+            key: fs.readFileSync(keyPath),
+            ca: [fs.readFileSync(rootCertPath)],
+          },
+        },
       },
     };
   }
