82ad858d699ae8009c57a08b94579ebe907ccf22
Stepan <Dark_Magic419a@mail.ru>
2024-11-15 08:59:39 +0300

    refactor: user-service remove jwt-service service-token

--- user-service/src/domain/service/jwt/jwt.service.ts
+++ user-service/src/domain/service/jwt/jwt.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { ServiceTokenView, UserTokenData } from './view';
+import { UserTokenData } from './view';
 import { ConfigService } from '@nestjs/config';
 import { JwtService as NestJwtService } from '@nestjs/jwt';
 import { DateService } from '../date';
@@ -22,32 +22,5 @@ export class JwtService {
       });
 
     return userData;
-  }
-
-  generateFileServiceToken(): ServiceTokenView {
-    const exp =
-      this.dateService.getUnixNow() +
-      this.configService.get<number>('JWT_FILE_SERVICE_EXPIRES_MS');
-
-    const accessToken = this.jwtService.sign(
-      { exp },
-      {
-        secret: this.configService.get<string>('JWT_FILE_SERVICE_SECRET'),
-      },
-    );
-
-    return { token: accessToken, exp };
-  }
-
-  async validateFileServiceToken(accessToken: string): Promise<boolean> {
-    await this.jwtService
-      .verifyAsync(accessToken, {
-        secret: this.configService.get<string>('JWT_FILE_SERVICE_SECRET'),
-      })
-      .catch(() => {
-        return false;
-      });
-
-    return true;
   }
 }
