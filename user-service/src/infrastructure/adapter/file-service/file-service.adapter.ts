fba638c568e871d449d930b160e9ea36cdb8db39
Stepan <Dark_Magic419a@mail.ru>
2024-11-15 08:59:17 +0300

    refactor: user-service remove file-service token-metadata

--- user-service/src/infrastructure/adapter/file-service/file-service.adapter.ts
+++ user-service/src/infrastructure/adapter/file-service/file-service.adapter.ts
@@ -18,22 +18,13 @@ import {
   UploadFileResponse,
 } from 'src/infrastructure/grpc/service/file/file.proto-service';
 import { firstValueFrom } from 'rxjs';
-import { Metadata } from '@grpc/grpc-js';
-import { JwtService, ServiceTokenView } from 'src/domain/service/jwt';
 import { UploadFileType } from 'src/domain/service/file';
-import { DateService } from 'src/domain/service/date';
 
 @Injectable()
 export class FileServiceAdapter implements OnModuleInit {
   private fileGrpcService: FileProtoService;
-  private serviceToken: ServiceTokenView;
-  private metadata: Metadata;
 
-  constructor(
-    @Inject(GRPC_SERVICE.FILE) private readonly client: ClientGrpc,
-    private readonly jwtService: JwtService,
-    private readonly dateService: DateService,
-  ) {}
+  constructor(@Inject(GRPC_SERVICE.FILE) private readonly client: ClientGrpc) {}
 
   private readonly logger = new Logger(FileServiceAdapter.name);
 
@@ -41,22 +32,6 @@ export class FileServiceAdapter implements OnModuleInit {
     this.fileGrpcService = this.client.getService<FileProtoService>(
       getGrpcPackageServiceName(GRPC_SERVICE.FILE),
     );
-  }
-
-  private getMetadata(): Metadata {
-    if (!this.metadata) {
-      this.metadata = new Metadata();
-    }
-
-    if (
-      !this.serviceToken ||
-      this.dateService.isUnixBeforeNow(this.serviceToken.exp)
-    ) {
-      this.serviceToken = this.jwtService.generateFileServiceToken();
-      this.metadata.set('authorization', 'Bearer ' + this.serviceToken.token);
-    }
-
-    return this.metadata;
   }
 
   async uploadFile(
@@ -77,9 +52,7 @@ export class FileServiceAdapter implements OnModuleInit {
       type: type,
     };
 
-    return firstValueFrom(
-      this.fileGrpcService.uploadFile(req, this.getMetadata()),
-    ).catch((err) => {
+    return firstValueFrom(this.fileGrpcService.uploadFile(req)).catch((err) => {
       this.logger.error(err.message, err.stack);
       throw new InternalServerErrorException();
     });
@@ -88,9 +61,7 @@ export class FileServiceAdapter implements OnModuleInit {
   deleteFile(filename: string): Promise<DeleteFileResponse> {
     const req: DeleteFileRequest = { filename };
 
-    return firstValueFrom(
-      this.fileGrpcService.deleteFile(req, this.getMetadata()),
-    ).catch((err) => {
+    return firstValueFrom(this.fileGrpcService.deleteFile(req)).catch((err) => {
       this.logger.error(err.message, err.stack);
       throw new InternalServerErrorException();
     });
