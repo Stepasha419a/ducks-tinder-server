931a797173d62d06673e08777d6aafc2c8f02f8e
Stepan <Dark_Magic419a@mail.ru>
2024-11-17 20:59:24 +0300

    feature: subscription-service config-service-impl

---
+++ subscription-service/internal/infrastructure/service/config_impl/config_impl.go
@@ -0,0 +1,84 @@
+package config_service_impl
+
+import (
+	"log"
+	"os"
+	"path"
+	"slices"
+
+	config_service "github.com/Stepasha419a/ducks-tinder-server/subscription-service/internal/domain/service/config"
+	validator_service "github.com/Stepasha419a/ducks-tinder-server/subscription-service/internal/domain/service/validator"
+
+	"github.com/jinzhu/copier"
+	"github.com/joho/godotenv"
+	"gopkg.in/yaml.v3"
+)
+
+type (
+	parsedConfig struct {
+		Mode                         string `validate:"required" yaml:"mode"`
+		JwtAccessSecret              string `validate:"required" yaml:"jwt_access_secret"`
+		JwtSubscriptionServiceSecret string `validate:"required" yaml:"jwt_subscription_service_secret"`
+		JwtBillingServiceSecret      string `validate:"required" yaml:"jwt_billing_service_secret"`
+		Port                         uint16 `validate:"required" yaml:"port"`
+		GrpcPort                     uint16 `validate:"required" yaml:"grpc_port"`
+		DatabaseUrl                  string `validate:"required" yaml:"database_url"`
+		ClientUrl                    string `validate:"required" yaml:"client_url"`
+		GrpcBillingServiceUrl        string `validate:"required" yaml:"grpc_billing_service_url"`
+		GrpcBillingServiceDomain     string `validate:"required" yaml:"grpc_billing_service_domain"`
+		LoginServiceUrl              string `validate:"required" yaml:"login_service_url"`
+	}
+
+	ConfigServiceImpl struct{}
+)
+
+var (
+	mods                          = []string{"dev", "dev-docker"}
+	config *config_service.Config = &config_service.Config{}
+)
+
+func NewConfigService(validatorService validator_service.ValidatorService) *ConfigServiceImpl {
+	log.Println("new config service")
+
+	requireConfig(validatorService)
+
+	return &ConfigServiceImpl{}
+}
+
+func requireConfig(validatorService validator_service.ValidatorService) {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		panic("Error loading .env file")
+	}
+
+	mode := os.Getenv("MODE")
+	if !slices.Contains(mods, mode) {
+		panic("Error unknown config mode")
+	}
+
+	filename := mode + ".yaml"
+	file, err := os.Open(path.Join("config", filename))
+
+	if err != nil || file == nil {
+		panic("Error loading " + filename + " config file")
+	}
+	defer file.Close()
+	decoder := yaml.NewDecoder(file)
+
+	parsedConfig := parsedConfig{}
+	if err := decoder.Decode(&parsedConfig); err != nil {
+		panic(err)
+	}
+
+	err = validatorService.Struct(parsedConfig)
+	if err != nil {
+		panic(err)
+	}
+
+	copier.Copy(config, &parsedConfig)
+}
+
+func (c *ConfigServiceImpl) GetConfig() *config_service.Config {
+	return config
+}
