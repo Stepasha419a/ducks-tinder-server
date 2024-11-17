3c9c961416fdf4022f74b356a2180a9ce0327fa8
Stepan <Dark_Magic419a@mail.ru>
2024-11-17 21:01:17 +0300

    feature: subscription-service subscription-repository-impl save

--- subscription-service/internal/infrastructure/repository_impl/subscription_repository_impl.go
+++ subscription-service/internal/infrastructure/repository_impl/subscription_repository_impl.go
@@ -20,6 +20,39 @@ func NewSubscriptionRepository(pg *database.PostgresInstance) *SubscriptionRepos
 	return &SubscriptionRepositoryImpl{pg}
 }
 
+func (r *SubscriptionRepositoryImpl) Save(ctx context.Context, subscription *entity.Subscription, tx pgx.Tx) (*entity.Subscription, error) {
+	existingSubscription, err := r.Find(ctx, subscription.UserId, tx)
+	if err != nil {
+		return nil, err
+	}
+
+	if existingSubscription != nil {
+		_, err = database.Exec(r.pg.Pool, tx)(ctx, "UPDATE subscriptions SET subscription=@subscription, login=@login, expiresAt=@expires_at WHERE user_id=@user_id", &pgx.NamedArgs{
+			"subscription": subscription.Subscription,
+			"user_id":      subscription.UserId,
+			"login":        subscription.Login,
+			"expires_at":   subscription.ExpiresAt,
+		})
+		if err != nil {
+			return nil, err
+		}
+		return subscription, nil
+	}
+
+	_, err = database.Exec(r.pg.Pool, tx)(ctx, "INSERT INTO subscriptions (user_id, subscription, login, expires_at, created_at) VALUES (@user_id, @subscription, @login, @expires_at, @created_at)", &pgx.NamedArgs{
+		"user_id":      subscription.UserId,
+		"subscription": subscription.Subscription,
+		"login":        subscription.Login,
+		"expires_at":   subscription.ExpiresAt,
+		"created_at":   subscription.CreatedAt,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return subscription, nil
+}
+
 func (r *SubscriptionRepositoryImpl) Find(ctx context.Context, userId string, tx pgx.Tx) (*entity.Subscription, error) {
 	subscription := &entity.Subscription{}
 
