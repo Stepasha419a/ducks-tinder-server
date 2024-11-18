2c159d75e217df0cac0bf708195e37a644e910db
Stepan <Dark_Magic419a@mail.ru>
2024-11-18 08:36:15 +0300

    feature: subscription-service database

---
+++ subscription-service/internal/infrastructure/database/database.go
@@ -0,0 +1,37 @@
+package database
+
+import (
+	"context"
+	"log"
+	"sync"
+
+	config_service "github.com/Stepasha419a/ducks-tinder-server/subscription-service/internal/domain/service/config"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type PostgresInstance struct {
+	Pool *pgxpool.Pool
+}
+
+var (
+	pgInstance *PostgresInstance
+	pgOnce     sync.Once
+)
+
+func NewPostgresInstance(configService config_service.ConfigService) (*PostgresInstance, func()) {
+	pgOnce.Do(func() {
+		log.Println("new database postgres instance")
+
+		pool, err := pgxpool.New(context.TODO(), configService.GetConfig().DatabaseUrl)
+		if err != nil {
+			panic(err)
+		}
+
+		pgInstance = &PostgresInstance{pool}
+	})
+
+	return pgInstance, cleanUp
+}
