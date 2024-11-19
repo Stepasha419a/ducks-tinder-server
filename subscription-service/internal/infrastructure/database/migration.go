c41da3eb9274ff948999201cf735a73ab488332f
Stepan <Dark_Magic419a@mail.ru>
2024-11-19 08:59:18 +0300

    feature: subscription-service database-init-migration

--- subscription-service/internal/infrastructure/database/migration.go
+++ subscription-service/internal/infrastructure/database/migration.go
@@ -49,6 +49,7 @@ func submitMigration() bool {
 }
 
 func runMigrations(ctx context.Context, pg *PostgresInstance, configService config_service.ConfigService) {
+	checkMigration(initMigration(ctx, pg, configService))
 }
 
 func checkMigration(err error) {
@@ -56,4 +57,47 @@ func checkMigration(err error) {
 		log.Info("migration - failed")
 		panic(err)
 	}
+}
+
+func initMigration(ctx context.Context, pg *PostgresInstance, configService config_service.ConfigService) error {
+	log.Info("migration - init")
+
+	postgresInstanceUrl := strings.Split(configService.GetConfig().DatabaseUrl, "subscription-service")[0] + "postgres"
+
+	postgresInstance, err := pgx.Connect(ctx, postgresInstanceUrl)
+	if err != nil {
+		return err
+	}
+
+	defer postgresInstance.Close(ctx)
+
+	var dbExists bool
+	err = postgresInstance.QueryRow(ctx, `
+    SELECT EXISTS (
+        SELECT FROM pg_database WHERE datname = 'subscription-service'
+    )`).Scan(&dbExists)
+	if err != nil {
+		return err
+	}
+
+	if !dbExists {
+		_, err = postgresInstance.Exec(ctx, `CREATE DATABASE "subscription-service"`)
+		if err != nil {
+			return err
+		}
+	}
+
+	_, err = pg.Pool.Exec(ctx, `CREATE TABLE IF NOT EXISTS subscriptions (
+		user_id UUID PRIMARY KEY,
+		subscription VARCHAR(40) NOT NULL,
+		login VARCHAR(39) NOT NULL,
+		
+		expires_at timestamp(3) without time zone,
+		created_at timestamp(3) without time zone NOT NULL DEFAULT CURRENT_TIMESTAMP
+	)`)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
