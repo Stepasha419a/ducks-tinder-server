bb7b755cef17366c63e35242dddfe7204cd3fc1c
Stepan <Dark_Magic419a@mail.ru>
2024-11-23 09:26:06 +0300

    feature: subscription-service grpc-service-context-impl response-methods

--- subscription-service/internal/interface/grpc/context/service_context_impl.go
+++ subscription-service/internal/interface/grpc/context/service_context_impl.go
@@ -36,3 +36,31 @@ func (s *ServiceContextImpl[R]) Response(status int, body R) error {
 
 	return nil
 }
+
+func (s *ServiceContextImpl[R]) ErrorMessage(statusCode int, message string) error {
+	return status.Error(codes.Code(statusCode), message)
+}
+
+func (s *ServiceContextImpl[R]) BadRequest() error {
+	return status.Error(codes.InvalidArgument, BadRequest)
+}
+
+func (s *ServiceContextImpl[R]) Unauthorized() error {
+	return status.Error(codes.Unauthenticated, Unauthorized)
+}
+
+func (s *ServiceContextImpl[R]) NotFound() error {
+	return status.Error(codes.NotFound, NotFound)
+}
+
+func (s *ServiceContextImpl[R]) MethodNotAllowed() error {
+	return status.Error(codes.Unimplemented, Unimplemented)
+}
+
+func (s *ServiceContextImpl[R]) Conflict() error {
+	return status.Error(codes.AlreadyExists, Conflict)
+}
+
+func (s *ServiceContextImpl[R]) InternalServerError() error {
+	return status.Error(codes.Internal, InternalServerError)
+}
