5c54c7c94dc699fc8bccca4eb03af604f2532189
Stepan <Dark_Magic419a@mail.ru>
2024-11-26 20:49:36 +0300

    feature: subscription-service tls-service get-tls-config

--- subscription-service/internal/infrastructure/service/tls/tls.go
+++ subscription-service/internal/infrastructure/service/tls/tls.go
@@ -18,3 +18,34 @@ type TlsService struct {
 func NewTlsService(configService config_service.ConfigService) *TlsService {
 	return &TlsService{configService}
 }
+
+func (s *TlsService) GetConfig() *tls.Config {
+	certPath := path.Join("cert", s.configService.GetConfig().Mode, "certificate.pem")
+	privateKeyPath := path.Join("cert", s.configService.GetConfig().Mode, "private-key.pem")
+	caCertPath := path.Join("cert", s.configService.GetConfig().Mode, "ca.crt")
+
+	caCert, err := os.ReadFile(caCertPath)
+	if err != nil {
+		log.Error("failed to read CA certificate", log.Any("error", err))
+	}
+
+	caCertPool := x509.NewCertPool()
+	if !caCertPool.AppendCertsFromPEM(caCert) {
+		log.Error("failed to append CA certificate to pool")
+	}
+
+	tlsConfig := &tls.Config{
+		ClientCAs:    caCertPool,
+		ClientAuth:   tls.RequireAndVerifyClientCert,
+		MinVersion:   tls.VersionTLS12,
+		Certificates: make([]tls.Certificate, 1),
+	}
+
+	serverCert, err := tls.LoadX509KeyPair(certPath, privateKeyPath)
+	if err != nil {
+		log.Error("failed to load server certificate and key", log.Any("error", err))
+	}
+	tlsConfig.Certificates[0] = serverCert
+
+	return tlsConfig
+}
