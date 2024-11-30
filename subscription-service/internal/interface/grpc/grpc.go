259a30d00cfb324cee2e5ea9670547284b68321f
Stepan <Dark_Magic419a@mail.ru>
2024-11-30 20:48:04 +0300

    feature: subscription-service grpc keepalive

--- subscription-service/internal/interface/grpc/grpc.go
+++ subscription-service/internal/interface/grpc/grpc.go
@@ -15,6 +15,20 @@ import (
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/keepalive"
 )
+
+var kaep = keepalive.EnforcementPolicy{
+	MinTime:             5 * time.Second,
+	PermitWithoutStream: true,
+}
+
+var kasp = keepalive.ServerParameters{
+	MaxConnectionIdle:     5 * time.Minute,
+	MaxConnectionAge:      time.Minute,
+	MaxConnectionAgeGrace: 15 * time.Second,
+	Time:                  15 * time.Second,
+	Timeout:               1 * time.Second,
+}
+
 func NewGrpc(billingServer gen.SubscriptionServiceServer, interceptor *grpc_interceptor.GrpcInterceptor, tlsService *tls_service.TlsService) (*grpc.Server, func()) {
 	tlsConfig := tlsService.GetConfig()
 	creds := credentials.NewTLS(tlsConfig)
@@ -22,6 +36,9 @@ func NewGrpc(billingServer gen.SubscriptionServiceServer, interceptor *grpc_inte
 	opts := []grpc.ServerOption{
 		grpc.ChainUnaryInterceptor(interceptor.RecoveryUnaryInterceptor, interceptor.AuthUnaryInterceptor),
 		grpc.Creds(creds),
+
+		grpc.KeepaliveEnforcementPolicy(kaep),
+		grpc.KeepaliveParams(kasp),
 	}
 
 	server := grpc.NewServer(opts...)
