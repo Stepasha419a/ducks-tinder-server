89f66ec8215878659d440b4dc626675870ef9937
Stepan <Dark_Magic419a@mail.ru>
2024-12-01 21:08:25 +0300

    feature: subscription-service main setup

---
+++ subscription-service/cmd/main/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	grpc_interface "github.com/Stepasha419a/ducks-tinder-server/subscription-service/internal/interface/grpc"
+	fiber_impl "github.com/Stepasha419a/ducks-tinder-server/subscription-service/internal/interface/http/fiber"
+	"google.golang.org/grpc/keepalive"
+
+	"golang.org/x/sync/errgroup"
+)
+func main() {
+	container, cleaner, err := newContainer()
+	if err != nil {
+		panic(err)
+	}
+
+	setUpWithGracefulShutdown(container, cleaner)
+}
+
+func setUpWithGracefulShutdown(container *Container, cleaner func()) {
+}
+
+func initListeners(g *errgroup.Group, container *Container) {
+}
