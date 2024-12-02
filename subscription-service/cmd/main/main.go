9a2eb4c79350c60ecdedc5217d2b65fefa738812
Stepan <Dark_Magic419a@mail.ru>
2024-12-02 08:57:20 +0300

    feature: subscription-service main setup listeners

--- subscription-service/cmd/main/main.go
+++ subscription-service/cmd/main/main.go
@@ -6,14 +6,13 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
 
 	grpc_interface "github.com/Stepasha419a/ducks-tinder-server/subscription-service/internal/interface/grpc"
 	fiber_impl "github.com/Stepasha419a/ducks-tinder-server/subscription-service/internal/interface/http/fiber"
-	"google.golang.org/grpc/keepalive"
 
 	"golang.org/x/sync/errgroup"
 )
+
 func main() {
 	container, cleaner, err := newContainer()
 	if err != nil {
@@ -24,6 +23,9 @@ func main() {
 }
 
 func setUpWithGracefulShutdown(container *Container, cleaner func()) {
+	g, gCtx := errgroup.WithContext(mainCtx)
+
+	initListeners(g, container)
 }
 
 func initListeners(g *errgroup.Group, container *Container) {
