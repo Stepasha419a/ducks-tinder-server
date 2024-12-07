e15d2b7d362de3f3255b8ae84fbd0da009fb1b14
Stepan <Dark_Magic419a@mail.ru>
2024-12-07 21:36:31 +0300

    feature: billing-service http-interface tls-config

--- billing-service/internal/interface/http/fiber/fiber_impl.go
+++ billing-service/internal/interface/http/fiber/fiber_impl.go
@@ -2,13 +2,15 @@ package fiber_impl
 
 import (
 	config_service "billing-service/internal/domain/service/config"
+	tls_service "billing-service/internal/infrastructure/service/tls"
 	fiber_impl_context "billing-service/internal/interface/http/fiber/context"
 	"billing-service/internal/interface/http/middleware"
+	"crypto/tls"
 	"errors"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
-	"path"
 	"strconv"
 
 	"github.com/gofiber/fiber/v3"
@@ -60,13 +62,19 @@ func NewFiberApp(middleware *middleware.Middleware, configService config_service
 	}
 }
 
-func InitHttpListener(app *fiber.App, configService config_service.ConfigService) error {
-	certPath := path.Join("cert", configService.GetConfig().Mode, "certificate.pem")
-	privateKeyPath := path.Join("cert", configService.GetConfig().Mode, "private-key.pem")
-
+func InitHttpListener(app *fiber.App, configService config_service.ConfigService, tlsService *tls_service.TlsService) error {
 	port := strconv.Itoa(int(configService.GetConfig().Port))
 
-	err := app.Listen(fmt.Sprintf("0.0.0.0:%s", port), fiber.ListenConfig{CertFile: certPath, CertKeyFile: privateKeyPath})
+	ln, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%s", port))
+	if err != nil {
+		return err
+	}
+
+	tlsConfig := tlsService.GetConfig()
+
+	ln = tls.NewListener(ln, tlsConfig)
+
+	err = app.Listener(ln)
 	if err != nil {
 		return err
 	}
