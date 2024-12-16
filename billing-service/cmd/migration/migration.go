0a8fda8c617fc1671007960246192014bc462aaa
Stepan <Dark_Magic419a@mail.ru>
2024-12-16 08:33:01 +0300

    feature: billing-service auto-submit-migration flag

--- billing-service/cmd/migration/migration.go
+++ billing-service/cmd/migration/migration.go
@@ -4,11 +4,16 @@ import (
 	"billing-service/internal/infrastructure/database"
 	config_service_impl "billing-service/internal/infrastructure/service/config_impl"
 	validator_service_impl "billing-service/internal/infrastructure/service/validator_impl"
+	"flag"
 )
 
 func main() {
 	validatorServiceImpl := validator_service_impl.NewValidatorService()
 	configService := config_service_impl.NewConfigService(validatorServiceImpl)
+	tlsService := tls_service.NewTlsService(configService)
+
+	autoSubmit := flag.Bool("auto-submit", false, "submit migrations automatically")
+	flag.Parse()
 
 	db, cleanup := database.NewPostgresInstance(configService)
 	database.MigrateDB(db, configService)
