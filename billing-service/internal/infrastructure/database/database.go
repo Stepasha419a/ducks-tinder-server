4cbbb0855c79548608054ae71dcefc28b783c4fa
Stepan <Dark_Magic419a@mail.ru>
2024-12-17 09:04:03 +0300

    feature: billing-service database tls-connection

--- billing-service/internal/infrastructure/database/database.go
+++ billing-service/internal/infrastructure/database/database.go
@@ -2,7 +2,9 @@ package database
 
 import (
 	config_service "billing-service/internal/domain/service/config"
+	tls_service "billing-service/internal/infrastructure/service/tls"
 	"context"
+	"fmt"
 	"log"
 	"sync"
 
@@ -20,11 +22,22 @@ var (
 	pgOnce     sync.Once
 )
 
-func NewPostgresInstance(configService config_service.ConfigService) (*PostgresInstance, func()) {
+func NewPostgresInstance(configService config_service.ConfigService, tlsService *tls_service.TlsService) (*PostgresInstance, func()) {
 	pgOnce.Do(func() {
 		log.Println("new database postgres instance")
 
-		pool, err := pgxpool.New(context.TODO(), configService.GetConfig().DatabaseUrl)
+		tlsConfig := tlsService.GetConfig()
+		config := configService.GetConfig()
+
+		connectionString := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=require", config.PostgresHost, config.PostgresPort, config.PostgresUser, config.PostgresPassword, config.PostgresDatabase)
+		pgxConfig, err := pgxpool.ParseConfig(connectionString)
+		if err != nil {
+			panic(err)
+		}
+
+		pgxConfig.ConnConfig.TLSConfig = tlsConfig
+
+		pool, err := pgxpool.NewWithConfig(context.TODO(), pgxConfig)
 		if err != nil {
 			panic(err)
 		}
