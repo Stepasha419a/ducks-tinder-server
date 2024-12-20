3d1ca79b84d5a2c12f80b9fe99659b4441184240
Stepan <Dark_Magic419a@mail.ru>
2024-12-20 20:45:45 +0300

    feature: subscription-service config-service-impl postgres

--- subscription-service/internal/infrastructure/service/config_impl/config_impl.go
+++ subscription-service/internal/infrastructure/service/config_impl/config_impl.go
@@ -22,11 +22,16 @@ type (
 		JwtBillingServiceSecret      string `validate:"required" yaml:"jwt_billing_service_secret"`
 		Port                         uint16 `validate:"required" yaml:"port"`
 		GrpcPort                     uint16 `validate:"required" yaml:"grpc_port"`
-		DatabaseUrl                  string `validate:"required" yaml:"database_url"`
 		ClientUrl                    string `validate:"required" yaml:"client_url"`
 		GrpcBillingServiceUrl        string `validate:"required" yaml:"grpc_billing_service_url"`
 		GrpcBillingServiceDomain     string `validate:"required" yaml:"grpc_billing_service_domain"`
 		LoginServiceUrl              string `validate:"required" yaml:"login_service_url"`
+		PostgresHost                 string `validate:"required" yaml:"postgres_host"`
+		PostgresPort                 uint16 `validate:"required" yaml:"postgres_port"`
+		PostgresUser                 string `validate:"required" yaml:"postgres_user"`
+		PostgresPassword             string `validate:"required" yaml:"postgres_password"`
+		PostgresDatabase             string `validate:"required" yaml:"postgres_database"`
+		PostgresRootDatabase         string `validate:"required" yaml:"postgres_root_database"`
 	}
 
 	ConfigServiceImpl struct{}
