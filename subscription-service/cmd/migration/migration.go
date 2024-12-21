7dd04160f5d9f4d5eaf1ad39a8aeb7b0add58a93
Stepan <Dark_Magic419a@mail.ru>
2024-12-21 19:33:40 +0300

    feature: subscription-service migration pass tls-service

--- subscription-service/cmd/migration/migration.go
+++ subscription-service/cmd/migration/migration.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 
 	config_service_impl "github.com/Stepasha419a/ducks-tinder-server/subscription-service/internal/infrastructure/service/config_impl"
+	tls_service "github.com/Stepasha419a/ducks-tinder-server/subscription-service/internal/infrastructure/service/tls"
 	validator_service_impl "github.com/Stepasha419a/ducks-tinder-server/subscription-service/internal/infrastructure/service/validator_impl"
 
 	"github.com/Stepasha419a/ducks-tinder-server/subscription-service/internal/infrastructure/database"
@@ -12,6 +13,7 @@ import (
 func main() {
 	validatorServiceImpl := validator_service_impl.NewValidatorService()
 	configService := config_service_impl.NewConfigService(validatorServiceImpl)
+	tlsService := tls_service.NewTlsService(configService)
 
 	autoSubmit := flag.Bool("auto-submit", false, "submit migrations automatically")
 	flag.Parse()
