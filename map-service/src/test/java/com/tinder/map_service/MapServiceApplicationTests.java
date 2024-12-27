819db108bb1ca94926a0f671e7c39487988986aa
stepan <dark_magic419a@mail.ru>
2024-12-27 22:30:38 +0300

    feature: map-service init tests

---
+++ map-service/src/test/java/com/tinder/map_service/MapServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tinder.map_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles(profiles = "dev")
+class MapServiceApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
