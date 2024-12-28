05f0a5de5e7b348bd952e2aba96f67a0ad830af3
stepan <dark_magic419a@mail.ru>
2024-12-28 09:51:29 +0300

    feature: map-service location-entity init

--- map-service/src/main/java/com/tinder/map_service/model/Location.java
+++ map-service/src/main/java/com/tinder/map_service/model/Location.java
@@ -11,4 +11,16 @@ public class Location {
   private LocationCapitalEnum capital = null;
   private double latitude, longitude;
   private Date createdAt;
+
+  public Location(UUID id, String city, String country, String adminRegion,
+      @Nullable LocationCapitalEnum capital, double latitude, double longitude, Date createdAt) {
+    this.id = id;
+    this.city = city;
+    this.country = country;
+    this.adminRegion = adminRegion;
+    this.capital = capital;
+    this.latitude = latitude;
+    this.longitude = longitude;
+    this.createdAt = createdAt;
+  }
 }
