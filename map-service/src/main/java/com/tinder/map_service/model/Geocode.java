a445342271cb8fcada0b9af4f63d1779bdbdee4d
stepan <dark_magic419a@mail.ru>
2024-12-28 09:18:43 +0300

    feature: map-service geocode getters

--- map-service/src/main/java/com/tinder/map_service/model/Geocode.java
+++ map-service/src/main/java/com/tinder/map_service/model/Geocode.java
@@ -7,4 +7,12 @@ public class Geocode {
         this.name = name;
         this.address = address;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
 }
