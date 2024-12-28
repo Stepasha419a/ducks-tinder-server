161fda2d10d4bc521419e899bc9150d79554035a
stepan <dark_magic419a@mail.ru>
2024-12-28 09:18:35 +0300

    feature: map-service geocode

---
+++ map-service/src/main/java/com/tinder/map_service/model/Geocode.java
@@ -0,0 +1,10 @@
+package com.tinder.map_service.model;
+
+public class Geocode {
+    private String name, address;
+
+    public Geocode(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+}
