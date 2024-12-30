7cfa108584995db0bf47269501605c603b505743
stepan <dark_magic419a@mail.ru>
2024-12-30 21:56:11 +0300

    feature: map-service location-converter to-model-from-row

--- map-service/src/main/java/com/tinder/map_service/database/converter/LocationConverter.java
+++ map-service/src/main/java/com/tinder/map_service/database/converter/LocationConverter.java
@@ -11,4 +11,23 @@ import com.tinder.map_service.model.LocationCapitalEnum;
 public class LocationConverter {
     private static final SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
+    public static Location toModelFromRow(Object[] row) {
+        var id = UUID.fromString(row[0].toString());
+        var latitude = Double.parseDouble(row[1].toString());
+        var longitude = Double.parseDouble(row[2].toString());
+        var city = row[3].toString();
+        var country = row[4].toString();
+        var capital = row[5] != null ? LocationCapitalEnum.fromString(row[5].toString()) : null;
+        var adminRegion = row[6].toString();
+
+        var createdAt = new Date();
+        try {
+            createdAt = formatter.parse(row[7].toString());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return new Location(id, city, country, adminRegion, capital, latitude, longitude,
+                createdAt);
+    }
 }
