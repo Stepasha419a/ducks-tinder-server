20d30ddba768d80bdce72818cf499f12b722df83
stepan <dark_magic419a@mail.ru>
2024-12-30 21:57:37 +0300

    feature: map-service flyway-config init

---
+++ map-service/src/main/java/com/tinder/map_service/database/migration/FlywayConfig.java
@@ -0,0 +1,15 @@
+package com.tinder.map_service.database.migration;
+
+import java.sql.DriverManager;
+
+import org.flywaydb.core.Flyway;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Configuration
+public class FlywayConfig {
+}
