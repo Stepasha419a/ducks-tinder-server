eec0ee6f6bf4da0f09efd509338e02ae9be2d848
stepan <dark_magic419a@mail.ru>
2024-12-30 21:55:52 +0300

    feature: map-service location-converter

---
+++ map-service/src/main/java/com/tinder/map_service/database/converter/LocationConverter.java
@@ -0,0 +1,14 @@
+package com.tinder.map_service.database.converter;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+
+import com.tinder.map_service.model.Location;
+import com.tinder.map_service.model.LocationCapitalEnum;
+
+public class LocationConverter {
+    private static final SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+}
