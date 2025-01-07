5c42ce85031d2f816111e4d18fa3b51ac2436b0b
stepan <dark_magic419a@mail.ru>
2025-01-07 21:45:56 +0300

    feature: map-service location-repository-impl find-nearest-by-coords

--- map-service/src/main/java/com/tinder/map_service/database/repository_impl/LocationRepositoryImpl.java
+++ map-service/src/main/java/com/tinder/map_service/database/repository_impl/LocationRepositoryImpl.java
@@ -16,4 +16,28 @@ public class LocationRepositoryImpl implements LocationRepository {
 
 	@PersistenceContext
 	private EntityManager entityManager;
+
+	@Override
+	public Optional<Location> findNearestByCoords(double latitude, double longitude) {
+		String query =
+				"""
+						SELECT id, latitude, longitude, city, country, capital, admin_region, created_at,
+						6371 * 2 * asin(
+							sqrt(
+								power(sin(radians((latitude - :latitude) / 2)), 2) +
+								cos(radians(:latitude)) * cos(radians(latitude)) * power(sin(radians((longitude - :longitude) / 2)), 2)
+							)
+						) as distance
+						FROM locations
+						ORDER BY distance
+						LIMIT 1
+						""";
+
+		@SuppressWarnings("unchecked")
+		Optional<Location> location = entityManager.createNativeQuery(query)
+				.setParameter("latitude", latitude).setParameter("longitude", longitude).getResultList()
+				.stream().findFirst().map(entity -> LocationConverter.toModelFromRow((Object[]) entity));
+
+		return location;
+	}
 }
