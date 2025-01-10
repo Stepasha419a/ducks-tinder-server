cd0b375e4fb1fd084cd87b470a2cca0cfa2dd7bd
Stepasha419a <Dark_Magic419a@mail.ru>
2025-01-10 17:43:22 +0300

    feature: map-service init-application

---
+++ map-service/src/main/java/com/tinder/map_service/MapServiceApplication.java
@@ -0,0 +1,11 @@
+package com.tinder.map_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MapServiceApplication.class, args);
+	}
+}
