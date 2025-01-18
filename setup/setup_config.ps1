afdd534401d2dd98849715b6e0a81d4f1fdaec60
stepan <dark_magic419a@mail.ru>
2025-01-18 21:39:39 +0300

    refactor: setup setup-config replacements form certs, jdbc

--- setup/setup_config.ps1
+++ setup/setup_config.ps1
@@ -5,7 +5,12 @@ $javaServices = ("map-service/src/main")
 $yamlConfigServices = ("database-seeder")
 $onlyDevConfigServices = ("database-seeder")
 
-$localVarsReplacement = ("postgresql://prisma:prisma@postgres:5432/", "postgresql://prisma:prisma@127.0.0.1:5433/"), ("host.docker.internal", "0.0.0.0"), ("amqp://rabbitmq:5672", "amqp://0.0.0.0:5672")
+$localVarsReplacement = 
+("postgresql://prisma:prisma@postgres:5432/", "postgresql://prisma:prisma@127.0.0.1:5433/"), 
+("jdbc:postgresql://postgres:5432", "jdbc:postgresql://127.0.0.1:5433"), 
+("host.docker.internal", "0.0.0.0"), 
+("amqp://rabbitmq:5672", "amqp://0.0.0.0:5672"), 
+("cert/dev-docker", "cert/dev")
 
 $secretsReplacementEnvFilePath = "../.env.secrets"
 
