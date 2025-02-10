21ec3644f77aa9558f51ca3e9f19b0f9a4294a66
stepan <dark_magic419a@mail.ru>
2025-02-10 09:11:18 +0300

    feature: database-seeder map-service-seeder seedLocations

--- database-seeder/internal/service/seeder/map_service/map_service_postgres_seeder.go
+++ database-seeder/internal/service/seeder/map_service/map_service_postgres_seeder.go
@@ -26,3 +26,189 @@ type (
 		CreatedAt time.Time
 	}
 )
+func seedLocations(ctx context.Context, tx pgx.Tx) error {
+	rows, err := getData()
+	if err != nil {
+		return err
+	}
+
+	locations, err := prepareRows(rows)
+	if err != nil {
+		return err
+	}
+
+	log.Print("seed map service postgres - truncating tables")
+
+	_, err = tx.Exec(ctx, "TRUNCATE TABLE locations CASCADE")
+	if err != nil {
+		return err
+	}
+
+	log.Print("seed map service postgres - seeding")
+
+	err = insertQueryFromRows(ctx, tx, locations)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func insertQueryFromRows(ctx context.Context, tx pgx.Tx, locations *[]*Location) error {
+	query := `INSERT INTO locations (
+		id, 
+		latitude, 
+		longitude, 
+		city, 
+		country, 
+		admin_region, 
+		capital, 
+		created_at
+	) VALUES (
+		@id,
+		@latitude,
+		@longitude,
+		@city,
+		@country,
+		@admin_region,
+		@capital,
+		@created_at
+	)`
+
+	batch := &pgx.Batch{}
+	for _, location := range *locations {
+		var capital *string = nil
+		if location.Capital != "" {
+			capital = &location.Capital
+		}
+
+		args := pgx.NamedArgs{
+			"id":           location.Id,
+			"latitude":     location.Latitude,
+			"longitude":    location.Longitude,
+			"city":         location.City,
+			"country":      location.Country,
+			"admin_region": location.AdminRegion,
+			"capital":      capital,
+			"created_at":   location.CreatedAt,
+		}
+
+		batch.Queue(query, args)
+	}
+
+	results := tx.SendBatch(ctx, batch)
+	defer results.Close()
+
+	for range *locations {
+		_, err := results.Exec()
+		if err != nil {
+			return err
+		}
+	}
+
+	return results.Close()
+}
+
+func prepareRows(rows *string) (*[]*Location, error) {
+	list := []*Location{}
+
+	for _, row := range strings.Split(*rows, "\n")[1:] {
+		if len(row) == 0 {
+			continue
+		}
+
+		location, err := getLocationFromStringRow(row)
+		if err != nil {
+			return nil, err
+		}
+
+		list = append(list, location)
+	}
+
+	return &list, nil
+}
+
+func getLocationFromStringRow(row string) (*Location, error) {
+	data := getSeparatedRow(row)
+
+	city := getString(data[0])
+
+	latitude, err := getFloatFromString(data[2])
+	if err != nil {
+		return nil, err
+	}
+
+	longitude, err := getFloatFromString(data[3])
+	if err != nil {
+		return nil, err
+	}
+
+	country := getString(data[4])
+	AdminRegion := getString(data[7])
+	capital := getString(data[8])
+
+	return &Location{
+		Id:          uuid.NewString(),
+		Latitude:    latitude,
+		Longitude:   longitude,
+		City:        city,
+		Country:     country,
+		AdminRegion: AdminRegion,
+		Capital:     capital,
+		CreatedAt:   time.Now(),
+	}, nil
+}
+
+func getSeparatedRow(row string) []string {
+	separatedRow := []string{}
+
+	start := 0
+	inString := false
+
+	for i, symbol := range row {
+		if symbol == ',' {
+			start = i + 1
+			continue
+		}
+
+		if symbol == '"' {
+			inString = !inString
+		}
+
+		if !inString && i != 0 {
+			separatedRow = append(separatedRow, row[start:i+1])
+		}
+
+		if len(separatedRow) == 11 {
+			break
+		}
+	}
+
+	return separatedRow
+}
+
+func getFloatFromString(value string) (float64, error) {
+	return strconv.ParseFloat(getString(value), 64)
+}
+
+func getString(value string) string {
+	return strings.ReplaceAll(value, "\"", "")
+}
+
+func getData() (*string, error) {
+	filePath := "data/map_service_postgres/locations.csv"
+	_, err := os.Stat(filePath)
+	if err != nil {
+		log.Print("seed map service postgres - no data file provided")
+		return nil, err
+	}
+
+	bytes, err := os.ReadFile("data/map_service_postgres/locations.csv")
+	if err != nil {
+		return nil, err
+	}
+
+	rows := string(bytes)
+
+	return &rows, nil
+}
