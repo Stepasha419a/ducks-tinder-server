bdb963bb207d7f46c06633811e87c1265e0475dc
stepan <dark_magic419a@mail.ru>
2025-02-11 09:05:43 +0300

    feature: database-seeder feed map-service

--- database-seeder/internal/service/seeder/seeder.go
+++ database-seeder/internal/service/seeder/seeder.go
@@ -4,6 +4,7 @@ import (
 	database_service "database-seeder/internal/service/database"
 	auth_service_seeder "database-seeder/internal/service/seeder/auth_service"
 	billing_service_seeder "database-seeder/internal/service/seeder/billing_service"
+	map_service_seeder "database-seeder/internal/service/seeder/map_service"
 	prisma_seeder "database-seeder/internal/service/seeder/prisma"
 	"log"
 )
@@ -24,6 +25,12 @@ func Seed(databaseInstances *database_service.DatabaseInstances) {
 	err = prisma_seeder.SeedPrismaPostgres(databaseInstances.PrismaPostgresInstance)
 	if err != nil {
 		log.Print("seed prisma postgres - error")
+		panic(err)
+	}
+
+	err = map_service_seeder.SeedMapServicePostgres(databaseInstances.MapServicePostgresInstance)
+	if err != nil {
+		log.Print("seed map service postgres - error")
 		panic(err)
 	}
 }
