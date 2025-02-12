b983b1b3d5826644bded73b5f8fcd89f0cc327b4
stepan <dark_magic419a@mail.ru>
2025-02-12 19:20:02 +0300

    refactor: chat-service disable tls

--- chat-service/src/main.ts
+++ chat-service/src/main.ts
@@ -18,8 +18,9 @@ async function bootstrap() {
     ca: fs.readFileSync(rootCertPath).toString(),
     key: fs.readFileSync(keyPath).toString(),
     cert: fs.readFileSync(certPath).toString(),
-    rejectUnauthorized: true,
-    requestCert: true,
+    // TODO: fix k8s ssl
+    /* rejectUnauthorized: true,
+    requestCert: true, */
   };
 
   const app = await NestFactory.create(ChatModule, { httpsOptions });
