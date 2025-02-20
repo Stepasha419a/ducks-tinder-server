097bfe04c29d00a7ecd2adf569a24a92da30903f
stepan <dark_magic419a@mail.ru>
2025-02-20 22:09:14 +0300

    refactor: map-service remove default tests-profile

--- map-service/src/test/java/com/tinder/map_service/MapServiceApplicationTests.java
+++ map-service/src/test/java/com/tinder/map_service/MapServiceApplicationTests.java
@@ -2,12 +2,9 @@ package com.tinder.map_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles(profiles = "dev")
 class MapServiceApplicationTests {
 	@Test
-	void contextLoads() {
-	}
+	void contextLoads() {}
 }
