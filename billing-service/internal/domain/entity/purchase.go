f84ef50bd736aa6bd7d61a29d9eb4d4a8fd4b71c
stepan <dark_magic419a@mail.ru>
2025-02-26 22:49:46 +0300

    refactor: billing-service purchase userId field

--- billing-service/internal/domain/entity/purchase.go
+++ billing-service/internal/domain/entity/purchase.go
@@ -9,14 +9,16 @@ import (
 type Purchase struct {
 	Id           string
 	CreditCardId string
+	UserId       string
 	Amount       int64
 
 	CreatedAt time.Time
 }
 
-func NewPurchase(creditCardId string, amount int64) (*Purchase, error) {
+func NewPurchase(userId string, creditCardId string, amount int64) (*Purchase, error) {
 	return &Purchase{
 		Id:           uuid.New().String(),
+		UserId:       userId,
 		CreditCardId: creditCardId,
 		Amount:       amount,
 
