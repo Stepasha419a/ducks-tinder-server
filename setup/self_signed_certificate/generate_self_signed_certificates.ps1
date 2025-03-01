459b1aa9eab45e3b4669ac2341b0a764e9b8e232
stepan <dark_magic419a@mail.ru>
2025-03-01 22:03:38 +0300

    feature: setup setup-certs generate-self-signed export client-identity

--- setup/self_signed_certificate/generate_self_signed_certificates.ps1
+++ setup/self_signed_certificate/generate_self_signed_certificates.ps1
@@ -6,7 +6,8 @@ if (-not (Get-Command "openssl" -ErrorAction SilentlyContinue)) {
 $commands = @(
     'openssl req -x509 -new -nodes -keyout cert/ca.key -out cert/ca.crt -days 365 -subj "/C=GB/ST=State/L=City/O=Organization/CN=MyCA"',
     "openssl req -new -nodes -newkey rsa:2048 -keyout cert/tls.key -out cert/server.csr -config config.cfg",
-    "openssl x509 -req -in cert/server.csr -CA cert/ca.crt -CAkey cert/ca.key -CAcreateserial -out cert/tls.crt -days 365 -extensions v3_req -extfile config.cfg"
+    "openssl x509 -req -in cert/server.csr -CA cert/ca.crt -CAkey cert/ca.key -CAcreateserial -out cert/tls.crt -days 365 -extensions v3_req -extfile config.cfg",
+    "openssl pkcs12 -export -out cert/client-identity.p12 -inkey cert/tls.key -in cert/tls.crt",
 )
 
 New-Item -ItemType Directory -Force -Path ./cert
