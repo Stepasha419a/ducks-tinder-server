8c1245dd0f723780e6a6afd73d190ab27ba6751a
stepan <dark_magic419a@mail.ru>
2025-03-02 21:54:02 +0300

    refactor: setup setup-certs generate-self-signed pkcs8 tls-key outforming

--- setup/self_signed_certificate/generate_self_signed_certificates.ps1
+++ setup/self_signed_certificate/generate_self_signed_certificates.ps1
@@ -7,8 +7,8 @@ $commands = @(
     'openssl req -x509 -new -nodes -keyout cert/ca.key -out cert/ca.crt -days 365 -subj "/C=GB/ST=State/L=City/O=Organization/CN=MyCA"',
     "openssl req -new -nodes -newkey rsa:2048 -keyout cert/tls.key -out cert/server.csr -config config.cfg",
     "openssl x509 -req -in cert/server.csr -CA cert/ca.crt -CAkey cert/ca.key -CAcreateserial -out cert/tls.crt -days 365 -extensions v3_req -extfile config.cfg",
-    "openssl pkcs12 -export -out cert/client-identity.p12 -inkey cert/tls.key -in cert/tls.crt",
-    "openssl rsa -in cert/tls.key -inform PEM -out cert/tls-key.pk8 -outform DER",
+    "openssl pkcs12 -export -out cert/client-identity.p12 -inkey cert/tls.key -in cert/tls.crt -certfile cert/ca.crt -name `"postgresql-client`" -password pass:password",
+    "openssl pkcs8 -topk8 -inform PEM -outform DER -in cert/tls.key -out cert/tls-key.der -nocrypt",
     "openssl x509 -in cert/tls.crt -out cert/tls-crt.der -outform DER",
     "openssl x509 -in cert/ca.crt -out cert/ca-crt.der -outform DER"
 )
