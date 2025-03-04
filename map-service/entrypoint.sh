2e48f0c7d2b80f868faaee633f6bf6229d0f4cd5
stepan <dark_magic419a@mail.ru>
2025-03-04 09:07:39 +0300

    refactor: map-service entrypoint remove tls-key-der if exists

--- map-service/entrypoint.sh
+++ map-service/entrypoint.sh
@@ -12,10 +12,17 @@ KEYSTORE_PASSWORD="${KEYSTORE_PASSWORD:-password}"
 # resources cert path duplicate same files => verify only one of them
 TRUSTSTORE_PATH="/tmp/truststore.jks"
 TLS_KEY_DER_PATH="/tmp/tls-key.der"
+
 CERT_PATH="/app/cert/$ACTIVE_PROFILE/ca.crt"
+TLS_KEY_PATH="/app/cert/$ACTIVE_PROFILE/tls.key"
+
 if [ -f "$TRUSTSTORE_PATH" ]; then
     echo "Removing existing truststore..."
     rm "$TRUSTSTORE_PATH"
+fi
+if [ -f "$TLS_KEY_DER_PATH" ]; then
+    echo "Removing existing tls-key-der..."
+    rm "$TLS_KEY_DER_PATH"
 fi
 
 echo "Importing CA certificate to Java truststore..."
