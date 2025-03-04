f6ae1f982c0d88d99575a15d23768fc114fd6504
stepan <dark_magic419a@mail.ru>
2025-03-04 09:10:05 +0300

    refactor: setup setup-certs setUpCert func

--- setup/setup_certificates.ps1
+++ setup/setup_certificates.ps1
@@ -6,21 +6,33 @@ param (
 
 $services = ("user-service", "chat-service", "auth-service", "billing-service", "file-service", "subscription-service", "prometheus", "grafana", "rabbitmq", "map-service", "database")
 
-foreach ($service in $services) {
-    $servicePath = $rootPath + "$service"
-    $serviceCertPath = $servicePath + "/cert"
-    $serviceCertExamplePath = $servicePath + "/cert-example"
+foreach ($service in $service")
+
+
+function SetUpCert {
+    param (
+        $certRootPath
+    )
+    
+    $serviceCertPath = $certRootPath + "/cert"
+    $serviceCertExamplePath = $certRootPath + "/cert-example"
 
     if ($override) {
         if (Test-Path -Path $serviceCertPath) {
             Remove-Item -LiteralPath $serviceCertPath -Force -Recurse
         }
         
-        Copy-item -Force -Recurse -Verbose $certExamplePath -Destination $servicePath
+        Copy-item -Force -Recurse -Verbose $certExamplePath -Destination $certRootPath
         Rename-Item -Path $serviceCertExamplePath -NewName "cert"
     }
     elseif (-Not (Test-Path -Path $serviceCertPath)) {
-        Copy-item -Force -Recurse -Verbose $certExamplePath -Destination $servicePath
+        Copy-item -Force -Recurse -Verbose $certExamplePath -Destination $certRootPath
         Rename-Item -Path $serviceCertExamplePath -NewName "cert"
     }
+}
+
+foreach ($service in $services) {
+    $servicePath = $rootPath + $service
+
+    SetUpCert($servicePath)
 }
