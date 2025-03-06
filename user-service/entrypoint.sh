94f7d05624ea18b187ccc4ab65972dcd8bf53e21
stepan <dark_magic419a@mail.ru>
2025-03-06 20:39:12 +0300

    feature: user-service entrypoint env-vars

--- user-service/entrypoint.sh
+++ user-service/entrypoint.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -e
 
+NODE_ENV="${NODE_ENV:-dev-docker}"
+CLIENT_IDENTITY_PASSWORD="${CLIENT_IDENTITY_PASSWORD:-password}"
+
+CLIENT_IDENTITY_PATH="/tmp/client-identity.p12"
+
+CERT_PATH="/usr/src/app/cert/$NODE_ENV/ca.crt"
+TLS_CERT_PATH="/usr/src/app/cert/$NODE_ENV/tls.crt"
+TLS_KEY_PATH="/usr/src/app/cert/$NODE_ENV/tls.key"
 
 echo "Starting application..."
 npm run start:prod
