21b177ffad28994867fd262b67004f0c175e9080
stepan <dark_magic419a@mail.ru>
2025-03-06 20:39:49 +0300

    feature: user-service entrypoint generate client-identity

--- user-service/entrypoint.sh
+++ user-service/entrypoint.sh
@@ -10,5 +10,8 @@ CERT_PATH="/usr/src/app/cert/$NODE_ENV/ca.crt"
 TLS_CERT_PATH="/usr/src/app/cert/$NODE_ENV/tls.crt"
 TLS_KEY_PATH="/usr/src/app/cert/$NODE_ENV/tls.key"
 
+echo "Creating client-identity..."
+openssl pkcs12 -export -out $CLIENT_IDENTITY_PATH -inkey $TLS_KEY_PATH -in $TLS_CERT_PATH -certfile $CERT_PATH -name "client-identity" -password pass:$CLIENT_IDENTITY_PASSWORD
+
 echo "Starting application..."
 npm run start:prod
