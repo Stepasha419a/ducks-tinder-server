d29f61744871fad42250769d67a81434d272467b
stepan <dark_magic419a@mail.ru>
2025-03-07 21:44:39 +0300

    refactor: user-service simplify db connection, use only db-url env

--- user-service/src/infrastructure/database/database.service.ts
+++ user-service/src/infrastructure/database/database.service.ts
@@ -1,22 +1,14 @@
 import { Injectable, OnModuleInit, INestApplication } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
-import * as path from 'path';
 
 @Injectable()
 export class DatabaseService extends PrismaClient implements OnModuleInit {
   constructor(private readonly configService: ConfigService) {
-    const identityPath = path.resolve(
-      configService.get('CLIENT_IDENTITY_PATH'),
-    );
-    const sslPassword = configService.get('CLIENT_IDENTITY_PASSWORD');
-
-    const connection = `${configService.get('DATABASE_URL')}?sslmode=verify-full&sslidentity=${identityPath}&sslpassword=${sslPassword}`;
-
     super({
       datasources: {
         db: {
-          url: connection,
+          url: configService.get('DATABASE_URL'),
         },
       },
     });
