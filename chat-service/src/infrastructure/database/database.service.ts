4af7d799ee92935ba61a60eeed9c9471331bfa2b
stepan <dark_magic419a@mail.ru>
2025-03-08 17:43:36 +0300

    feature: chat-service database client-identity connection

--- chat-service/src/infrastructure/database/database.service.ts
+++ chat-service/src/infrastructure/database/database.service.ts
@@ -1,14 +1,23 @@
 import { Injectable, OnModuleInit, INestApplication } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
+import * as path from 'path';
 
 @Injectable()
 export class DatabaseService extends PrismaClient implements OnModuleInit {
   constructor(private readonly configService: ConfigService) {
+    const mode = configService.get('NODE_ENV');
+    const certsPath = path.resolve(`./cert/${mode}`);
+
+    const identityPath = `${certsPath}/client-identity.p12`;
+    const sslPassword = configService.get('SSL_PASSWORD');
+
+    const connection = `${configService.get('DATABASE_URL')}?sslmode=verify-full&sslidentity=${identityPath}&sslpassword=${sslPassword}`;
+
     super({
       datasources: {
         db: {
-          url: configService.get('DATABASE_URL'),
+          url: connection,
         },
       },
     });
