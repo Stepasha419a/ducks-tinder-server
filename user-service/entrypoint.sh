260080263483b986a3d905a01aac5d8c9244afd2
stepan <dark_magic419a@mail.ru>
2025-03-08 14:55:09 +0300

    feature: user-service entrypoint migration-db-name env, create db if not exists

--- user-service/entrypoint.sh
+++ user-service/entrypoint.sh
@@ -9,6 +9,7 @@ fi
 
 NODE_ENV="${NODE_ENV:-dev-docker}"
 IS_MIGRATION="${IS_MIGRATION:-false}"
+MIGRATION_DB_NAME="${MIGRATION_DB_NAME}"
 CLIENT_IDENTITY_PASSWORD="${CLIENT_IDENTITY_PASSWORD:-password}"
 
 CLIENT_IDENTITY_PATH="/tmp/client-identity.p12"
@@ -25,7 +26,11 @@ fi
 echo "Creating client-identity..."
 openssl pkcs12 -export -out $CLIENT_IDENTITY_PATH -inkey $TLS_KEY_PATH -in $TLS_CERT_PATH -certfile $CERT_PATH -name "client-identity" -password pass:$CLIENT_IDENTITY_PASSWORD
 
-if [[ "$IS_MIGRATION_LOWER" == "true" ]]; then
+if [[ "$IS_MIGRATION" == "true" && -n "$MIGRATION_DB_NAME" ]]; then
+    echo "Ensuring database exists..."
+    PGPASSWORD=prisma psql "postgresql://postgres:prisma@postgres-service:5432/postgres?sslmode=verify-full&sslrootcert=$CERT_PATH&sslcert=$TLS_CERT_PATH&sslkey=$TLS_KEY_PATH" -tc "SELECT 1 FROM pg_database WHERE datname = '$MIGRATION_DB_NAME'" | grep -q 1 || \
+    PGPASSWORD=prisma psql "postgresql://postgres:prisma@postgres-service:5432/postgres?sslmode=verify-full&sslrootcert=$CERT_PATH&sslcert=$TLS_CERT_PATH&sslkey=$TLS_KEY_PATH" -c "CREATE DATABASE $MIGRATION_DB_NAME;"
+
     echo "Migrating database..."
     npx prisma migrate deploy
 else
