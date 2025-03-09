56d5454aae060b8c4c092123e7d838bc3ace1dd4
stepan <dark_magic419a@mail.ru>
2025-03-09 20:48:05 +0300

    feature: chat-service entrypoint create client-identity, start app

--- chat-service/entrypoint.sh
+++ chat-service/entrypoint.sh
@@ -11,3 +11,8 @@ CLIENT_IDENTITY_PATH="/tmp/client-identity.p12"
 CERT_PATH="/usr/src/app/cert/$NODE_ENV/ca.crt"
 TLS_CERT_PATH="/usr/src/app/cert/$NODE_ENV/tls.crt"
 TLS_KEY_PATH="/usr/src/app/cert/$NODE_ENV/tls.key"
+echo "Creating client-identity..."
+openssl pkcs12 -export -out $CLIENT_IDENTITY_PATH -inkey $TLS_KEY_PATH -in $TLS_CERT_PATH -certfile $CERT_PATH -name "client-identity" -password pass:$CLIENT_IDENTITY_PASSWORD
+
+    echo "Starting application..."
+    npm run start:prod
