58dcb946386150fafb0f50956b8cc50e4d3ab422
stepan <dark_magic419a@mail.ru>
2025-03-10 09:00:51 +0300

    feature: chat-service entrypoint remove existing client-identity

--- chat-service/entrypoint.sh
+++ chat-service/entrypoint.sh
@@ -11,6 +11,12 @@ CLIENT_IDENTITY_PATH="/tmp/client-identity.p12"
 CERT_PATH="/usr/src/app/cert/$NODE_ENV/ca.crt"
 TLS_CERT_PATH="/usr/src/app/cert/$NODE_ENV/tls.crt"
 TLS_KEY_PATH="/usr/src/app/cert/$NODE_ENV/tls.key"
+
+if [ -f "$CLIENT_IDENTITY_PATH" ]; then
+    echo "Removing existing client-identity..."
+    rm "$CLIENT_IDENTITY_PATH"
+fi
+
 echo "Creating client-identity..."
 openssl pkcs12 -export -out $CLIENT_IDENTITY_PATH -inkey $TLS_KEY_PATH -in $TLS_CERT_PATH -certfile $CERT_PATH -name "client-identity" -password pass:$CLIENT_IDENTITY_PASSWORD
 
