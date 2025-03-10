c462bce0c4e8b4f1ee243cee2e434f8262d37dd8
stepan <dark_magic419a@mail.ru>
2025-03-10 08:58:33 +0300

    feature: chat-service entrypoint migration-db-name env, create db if not exists

--- chat-service/entrypoint.sh
+++ chat-service/entrypoint.sh
@@ -14,5 +14,14 @@ TLS_KEY_PATH="/usr/src/app/cert/$NODE_ENV/tls.key"
 echo "Creating client-identity..."
 openssl pkcs12 -export -out $CLIENT_IDENTITY_PATH -inkey $TLS_KEY_PATH -in $TLS_CERT_PATH -certfile $CERT_PATH -name "client-identity" -password pass:$CLIENT_IDENTITY_PASSWORD
 
+if [[ "$IS_MIGRATION" == "true" && -n "$MIGRATION_DB_NAME" ]]; then
+    echo "Ensuring database exists..."
+    PGPASSWORD=prisma psql "postgresql://postgres:prisma@postgres-service:5432/postgres?sslmode=verify-full&sslrootcert=$CERT_PATH&sslcert=$TLS_CERT_PATH&sslkey=$TLS_KEY_PATH" -tc "SELECT 1 FROM pg_database WHERE datname = '$MIGRATION_DB_NAME'" | grep -q 1 || \
+    PGPASSWORD=prisma psql "postgresql://postgres:prisma@postgres-service:5432/postgres?sslmode=verify-full&sslrootcert=$CERT_PATH&sslcert=$TLS_CERT_PATH&sslkey=$TLS_KEY_PATH" -c "CREATE DATABASE $MIGRATION_DB_NAME;"
+
+    echo "Migrating database..."
+    npx prisma migrate deploy
+else
     echo "Starting application..."
     npm run start:prod
+fi
