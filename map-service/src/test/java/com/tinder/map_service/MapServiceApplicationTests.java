a6f3cc21486a4629d5d4251a49e1e8b11901bd73
stepan <dark_magic419a@mail.ru>
2025-10-25 10:12:35 +0300

    refactor: map-service specify tests profile

--- map-service/src/test/java/com/tinder/map_service/MapServiceApplicationTests.java
+++ map-service/src/test/java/com/tinder/map_service/MapServiceApplicationTests.java
@@ -3,7 +3,7 @@ package com.tinder.map_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.profiles.active=test")
 class MapServiceApplicationTests {
 	@Test
 	void contextLoads() {}
