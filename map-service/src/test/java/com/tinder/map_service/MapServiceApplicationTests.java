4b990713b25de57e3bc15c8ce4b6a918cd69561d
stepan <dark_magic419a@mail.ru>
2025-10-25 10:12:49 +0300

    refactor: map-service mock location-repository

--- map-service/src/test/java/com/tinder/map_service/MapServiceApplicationTests.java
+++ map-service/src/test/java/com/tinder/map_service/MapServiceApplicationTests.java
@@ -2,9 +2,15 @@ package com.tinder.map_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import com.tinder.map_service.database.repository_impl.LocationRepositoryImpl;
 
 @SpringBootTest(properties = "spring.profiles.active=test")
 class MapServiceApplicationTests {
+
+	@MockBean
+	private LocationRepositoryImpl locationRepository;
+
 	@Test
 	void contextLoads() {}
 }
