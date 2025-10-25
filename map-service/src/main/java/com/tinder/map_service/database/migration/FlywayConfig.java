36bab4bb3003e6f8e0da5b5d4046703a3e3151ed
stepan <dark_magic419a@mail.ru>
2025-10-25 10:12:13 +0300

    refactor: map-service avoid running flyway for tests

--- map-service/src/main/java/com/tinder/map_service/database/migration/FlywayConfig.java
+++ map-service/src/main/java/com/tinder/map_service/database/migration/FlywayConfig.java
@@ -5,9 +5,10 @@ import org.flywaydb.core.Flyway;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.context.annotation.Profile;
 import lombok.extern.slf4j.Slf4j;
 
+@Profile("!test")
 @Slf4j
 @Configuration
 public class FlywayConfig {
