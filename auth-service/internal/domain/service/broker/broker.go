c804db9d366cdf82d92aa306dcdd5668225f48fd
stepan <dark_magic419a@mail.ru>
2025-11-03 21:27:25 +0300

    feature: auth-service broker add queue connection

--- auth-service/internal/domain/service/broker/broker.go
+++ auth-service/internal/domain/service/broker/broker.go
@@ -19,51 +19,12 @@ type BrokerService struct {
 	queues            map[string]*QueueConnection
 }
 
-func InitBroker(tlsConfig *tls.Config) *amqp.Connection {
-	RABBIT_MQ_URL := config_service.GetConfig().RabbitMqUrl
-
-	if err != nil {
-		slog.Error("Broker cert error", slog.Any("err", err))
-	}
-
-	slog.Info("Broker connecting")
-
-	conn, err = amqp.DialTLS(RABBIT_MQ_URL, tlsConfig)
-	if err != nil {
-		slog.Error("Broker connection error, reconnecting", slog.Any("err", err))
-		reconnect(tlsConfig)
-	}
-
-	slog.Info("Broker successfully connected")
-
-	return conn
-}
-
-func reconnect(tlsConfig *tls.Config) {
-	attempts := 0
-	ticker := time.NewTicker(time.Second * 3)
-	RABBIT_MQ_URL := config_service.GetConfig().RabbitMqUrl
-
-	for range ticker.C {
-		attempts++
-		slog.Info("Broker connecting", slog.Int("attempts", attempts))
-		if attempts >= 10 {
-			slog.Error("Broker connection error", slog.Any("err", err))
-			panic(err)
-		}
-		slog.Error("Broker connection error, reconnecting", slog.Any("err", err))
-
-		conn, err = amqp.DialTLS(RABBIT_MQ_URL, tlsConfig)
-		if err == nil {
-			ticker.Stop()
-			break
-		}
-	}
-}
-
 type QueueConnection struct {
+	mu      sync.RWMutex
 	channel *amqp.Channel
 	queue   *amqp.Queue
+	name    string
+}
 }
 
 func InitQueue(conn *amqp.Connection, queueName string) *QueueConnection {
