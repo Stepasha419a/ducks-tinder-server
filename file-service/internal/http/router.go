ee625043ef3f16d79d83edb7ef1b00f04a69de86
stepan <dark_magic419a@mail.ru>
2025-11-08 10:58:43 +0300

    feature: file-service router health handlers

--- file-service/internal/http/router.go
+++ file-service/internal/http/router.go
@@ -1,6 +1,7 @@
 package http_service
 
 import (
+	"go-file-server/internal/handler"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -13,6 +14,10 @@ func InitRouter() *mux.Router {
 	metricsHandler := promhttp.Handler()
 
 	router.Path("/metrics").Handler(metricsHandler)
+
+	router.Path("/livez").HandlerFunc(handler.SuccessHealth)
+	router.Path("/readyz").HandlerFunc(handler.SuccessHealth)
+	router.Path("/startupz").HandlerFunc(handler.SuccessHealth)
 
 	router.PathPrefix("/").Handler(http.FileServer(http.Dir("../static"))).Methods("GET")
 
