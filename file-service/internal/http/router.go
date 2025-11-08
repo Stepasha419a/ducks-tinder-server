8c5e1692cb4e386f3f2eebaacf2d3a675da59f3b
stepan <dark_magic419a@mail.ru>
2025-11-08 10:57:29 +0300

    refactor: file-service router part of http-service

---
+++ file-service/internal/http/router.go
@@ -0,0 +1,20 @@
+package http_service
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+func InitRouter() *mux.Router {
+	router := mux.NewRouter()
+
+	metricsHandler := promhttp.Handler()
+
+	router.Path("/metrics").Handler(metricsHandler)
+
+	router.PathPrefix("/").Handler(http.FileServer(http.Dir("../static"))).Methods("GET")
+
+	return router
+}
