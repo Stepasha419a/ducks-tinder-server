99bb5565be5a82fc381307678022528f216c46dc
stepan <dark_magic419a@mail.ru>
2025-11-09 09:32:43 +0300

    feature: file-service http-service creator func

--- file-service/internal/http/http.go
+++ file-service/internal/http/http.go
@@ -1,9 +1,24 @@
 package http_service
 
 import (
+	"context"
+	"fmt"
+	config_service "go-file-server/internal/service/config"
+	tls_service "go-file-server/internal/service/tls"
+	"log"
 	"net/http"
 )
 
 type HttpService struct {
 	server *http.Server
+}
+
+func NewHttpService() *HttpService {
+	server := &http.Server{
+		Addr:      fmt.Sprintf("0.0.0.0:%d", config_service.GetConfig().Port),
+		Handler:   InitRouter(),
+		TLSConfig: tls_service.GetConfig(),
+	}
+
+	return &HttpService{server}
 }
