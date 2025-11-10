20137d1baae05f9c963c6991e3500a66ed5e6bfc
stepan <dark_magic419a@mail.ru>
2025-11-10 09:43:13 +0300

    feature: file-service write-file func

--- file-service/internal/service/file/file.go
+++ file-service/internal/service/file/file.go
@@ -1,8 +1,26 @@
 package file_service
 
 import (
+	config_service "go-file-server/internal/service/config"
 	"os"
+	"path"
 )
+
+func WriteFile(data []byte, filename string) error {
+	staticDirPath := config_service.GetConfig().StaticDirPath
+
+	err := ensureDir(staticDirPath)
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(path.Join(staticDirPath, filename), data, 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
 
 func ensureDir(path string) error {
 	err := os.MkdirAll(path, 0770)
