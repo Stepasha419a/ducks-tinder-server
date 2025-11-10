b7a9c9a691208e9178426623239999047406f387
stepan <dark_magic419a@mail.ru>
2025-11-10 09:15:54 +0300

    feature: file-service http-service shutdown func

--- file-service/internal/http/http.go
+++ file-service/internal/http/http.go
@@ -24,12 +24,23 @@ func NewHttpService() *HttpService {
 }
 
 func (hs *HttpService) ListenAndServeTLS() error {
-	log.Printf("serving on HTTPS port: %v\n", config_service.GetConfig().Port)
-
 	err := hs.server.ListenAndServeTLS("", "")
 	if err != nil && err != http.ErrServerClosed {
 		return fmt.Errorf("http server error: %w", err)
 	}
 
+	log.Printf("serving on HTTPS port: %v\n", config_service.GetConfig().Port)
+
+	return nil
+}
+
+func (hs *HttpService) Shutdown(ctx context.Context) error {
+	err := hs.server.Shutdown(ctx)
+
+	if err != nil {
+		return err
+	}
+
+	log.Println("HTTPS server stopped gracefully")
 	return nil
 }
