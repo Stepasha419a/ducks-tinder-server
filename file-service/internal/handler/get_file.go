40dad5a013f669ce6c818537ff98a99770ba3291
stepan <dark_magic419a@mail.ru>
2025-11-11 08:56:06 +0300

    feature: file-service get-file handler

--- file-service/internal/handler/get_file.go
+++ file-service/internal/handler/get_file.go
@@ -3,9 +3,24 @@ package handler
 import (
 	config_service "go-file-server/internal/service/config"
 	"net/http"
+	"os"
 )
 
 var globalFs http.Handler = nil
+
+func GetFile(w http.ResponseWriter, r *http.Request) {
+	staticDirPath := config_service.GetConfig().StaticDirPath
+	fs := getFs()
+	file := staticDirPath + r.URL.Path
+
+	info, err := os.Stat(file)
+	if err == nil && info.IsDir() {
+		http.Error(w, "Forbidden", http.StatusForbidden)
+		return
+	}
+
+	fs.ServeHTTP(w, r)
+}
 
 func getFs() http.Handler {
 	staticDirPath := config_service.GetConfig().StaticDirPath
