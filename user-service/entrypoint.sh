df25b725b1d3f062848cff3a811e49fc2a8c4b60
stepan <dark_magic419a@mail.ru>
2025-11-16 09:15:19 +0300

    refactor: user-service entrypoint certs path

--- user-service/entrypoint.sh
+++ user-service/entrypoint.sh
@@ -9,9 +9,9 @@ CLIENT_IDENTITY_PASSWORD="${CLIENT_IDENTITY_PASSWORD:-password}"
 
 CLIENT_IDENTITY_PATH="/tmp/client-identity.p12"
 
-CERT_PATH="/usr/src/app/cert/$NODE_ENV/ca.crt"
-TLS_CERT_PATH="/usr/src/app/cert/$NODE_ENV/tls.crt"
-TLS_KEY_PATH="/usr/src/app/cert/$NODE_ENV/tls.key"
+CERT_PATH="/app/cert/$NODE_ENV/ca.crt"
+TLS_CERT_PATH="/app/cert/$NODE_ENV/tls.crt"
+TLS_KEY_PATH="/app/cert/$NODE_ENV/tls.key"
 
 if [ -f "$CLIENT_IDENTITY_PATH" ]; then
     echo "Removing existing client-identity..."
