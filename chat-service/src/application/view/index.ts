4e244dc8068d57ce83464b242a64d70341e09511
stepan <dark_magic419a@mail.ru>
2025-11-18 21:48:16 +0300

    refactor: chat-service remove old view

--- chat-service/src/application/view/index.ts
+++ chat-service/src/application/view/index.ts
@@ -1,4 +1,3 @@
 export { MessagesPaginationView } from './messages-pagination.view';
 export { NewMessageView } from './new-message.view';
 export { NewMessagesCountView } from './new-messages-count.view';
-export { ChatMemberView } from './chat-member.view';
