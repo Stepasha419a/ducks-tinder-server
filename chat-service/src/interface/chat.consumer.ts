f9630d297e81f5212e99ded375f211c9ef5721b6
stepan <dark_magic419a@mail.ru>
2025-11-20 21:41:21 +0300

    feature: chat-service chat-consumer handle-create-chat logger

--- chat-service/src/interface/chat.consumer.ts
+++ chat-service/src/interface/chat.consumer.ts
@@ -7,15 +7,26 @@ import { RABBITMQ } from 'src/infrastructure/rabbitmq';
 @Injectable()
 export class ChatConsumer {
   constructor(private readonly facade: ChatFacade) {}
+
+  private readonly logger = new Logger(ChatConsumer.name);
+
   @RabbitSubscribe({
     exchange: RABBITMQ.CHAT.EXCHANGE,
     routingKey: RABBITMQ.CHAT.EVENTS.CREATE_CHAT,
     queue: RABBITMQ.CHAT.QUEUE,
   })
   public async handleCreateChatEvent(message: CreateChatDto) {
+    this.logger.log(
+      'Received message',
+      RABBITMQ.CHAT.EVENTS.CREATE_CHAT,
+      message,
+    );
+
     try {
       await this.facade.commands.createChat(message);
     } catch (error) {
+      this.logger.error('Failed to create chat', error);
+
       throw error;
     }
   }
