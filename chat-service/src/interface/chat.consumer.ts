c656bdc334bd249afd8ea5c1b43bd582ade9ddf2
stepan <dark_magic419a@mail.ru>
2025-11-20 21:40:56 +0300

    feature: chat-service chat-consumer handle-create-chat event

--- chat-service/src/interface/chat.consumer.ts
+++ chat-service/src/interface/chat.consumer.ts
@@ -1,5 +1,5 @@
 import { RabbitSubscribe } from '@golevelup/nestjs-rabbitmq';
-import { Injectable, Logger, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { ChatFacade } from 'src/application';
 import { CreateChatDto } from 'src/application/command';
 import { RABBITMQ } from 'src/infrastructure/rabbitmq';
@@ -7,4 +7,16 @@ import { RABBITMQ } from 'src/infrastructure/rabbitmq';
 @Injectable()
 export class ChatConsumer {
   constructor(private readonly facade: ChatFacade) {}
+  @RabbitSubscribe({
+    exchange: RABBITMQ.CHAT.EXCHANGE,
+    routingKey: RABBITMQ.CHAT.EVENTS.CREATE_CHAT,
+    queue: RABBITMQ.CHAT.QUEUE,
+  })
+  public async handleCreateChatEvent(message: CreateChatDto) {
+    try {
+      await this.facade.commands.createChat(message);
+    } catch (error) {
+      throw error;
+    }
+  }
 }
