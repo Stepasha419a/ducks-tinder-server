b86d89638580b8d32792b718b326e3f4657b5f47
stepan <dark_magic419a@mail.ru>
2025-11-22 16:07:28 +0300

    feature: chat-service user-api-implementation get-chat-memeber-view handler

--- chat-service/src/infrastructure/adapter/user-api/user-api.adapter-implementation.ts
+++ chat-service/src/infrastructure/adapter/user-api/user-api.adapter-implementation.ts
@@ -8,4 +8,22 @@ import { RABBITMQ } from 'src/infrastructure/rabbitmq';
 @Injectable()
 export class UserApiImplementation implements UserApi {
   constructor(private readonly amqp: AmqpConnection) {}
+
+  private readonly logger = new Logger(ChatConsumer.name);
+
+  async getChatMemberView(userId: string): Promise<ChatMemberView> {
+    try {
+      const view = await this.amqp.request<ChatMemberView>({
+        exchange: RABBITMQ.USER.EXCHANGE,
+        routingKey: RABBITMQ.USER.EVENTS.GET_SHORT_USER,
+        payload: userId,
+      });
+
+      return view;
+    } catch (error) {
+      this.logger.error('Failed to get chat member view', error);
+
+      throw error;
+    }
+  }
 }
