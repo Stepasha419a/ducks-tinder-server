ba83c853e71782495e4f7cad82124997e31106c5
stepan <dark_magic419a@mail.ru>
2025-11-24 08:24:57 +0300

    feature: user-service chat-api-implementation create-chat handler

--- user-service/src/infrastructure/adapter/chat-api/chat-api.adapter-implementation.ts
+++ user-service/src/infrastructure/adapter/chat-api/chat-api.adapter-implementation.ts
@@ -7,4 +7,20 @@ import { CreateChatDto } from 'src/application/user/adapter/chat-api/dto';
 @Injectable()
 export class ChatApiImplementation implements ChatApi {
   constructor(private readonly amqp: AmqpConnection) {}
+
+  private readonly logger = new Logger(ChatApiImplementation.name);
+
+  async createChat(dto: CreateChatDto) {
+    try {
+      await this.amqp.publish(
+        RABBITMQ.CHAT.EXCHANGE,
+        RABBITMQ.CHAT.EVENTS.CREATE_CHAT,
+        dto,
+      );
+    } catch (error) {
+      this.logger.error('Failed to send create chat event', error);
+
+      throw error;
+    }
+  }
 }
