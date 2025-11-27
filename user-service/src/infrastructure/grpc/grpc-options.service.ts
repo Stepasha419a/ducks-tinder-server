71bfe7b2fdbc7a8eb5d41464dee0f5603d7c1912
stepan <dark_magic419a@mail.ru>
2025-11-27 13:37:03 +0300

    feature: user-service grpc-options-service tls credentials

--- user-service/src/infrastructure/grpc/grpc-options.service.ts
+++ user-service/src/infrastructure/grpc/grpc-options.service.ts
@@ -1,7 +1,9 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { GrpcOptions, Transport } from '@nestjs/microservices';
 import { ConfigService } from '@nestjs/config';
+import { readFileSync } from 'fs';
 import * as path from 'path';
+import { ServerCredentials } from '@grpc/grpc-js';
 import { GRPC_SERVICE } from './service';
 
 @Injectable()
@@ -13,7 +15,29 @@ export class GrpcOptionsService {
   getOptions(): GrpcOptions {
     this.logger.log(`Initializing gRPC server: ${GRPC_SERVICE.USER}`);
 
+    const mode = this.configService.get<string>('NODE_ENV');
     const port = this.configService.get<string>('GRPC_PORT');
+
+    const rootCert = readFileSync(
+      path.join(process.cwd(), 'cert', mode, 'ca.crt'),
+    );
+    const certChain = readFileSync(
+      path.join(process.cwd(), 'cert', mode, 'tls.crt'),
+    );
+    const privateKey = readFileSync(
+      path.join(process.cwd(), 'cert', mode, 'tls.key'),
+    );
+
+    const credentials = ServerCredentials.createSsl(
+      rootCert,
+      [
+        {
+          cert_chain: certChain,
+          private_key: privateKey,
+        },
+      ],
+      true,
+    );
 
     return {
       transport: Transport.GRPC,
@@ -30,6 +54,7 @@ export class GrpcOptionsService {
           keepalivePermitWithoutCalls: 1,
         },
         protoPath: path.join(process.cwd(), 'proto/user.proto'),
+        credentials,
       },
     };
   }
