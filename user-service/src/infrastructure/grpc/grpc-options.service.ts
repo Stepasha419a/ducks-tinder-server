b1282a7b90f93cee3021d1b7f11d8c96004a4eac
stepan <dark_magic419a@mail.ru>
2025-11-27 13:36:36 +0300

    feature: user-service grpc-options-service

---
+++ user-service/src/infrastructure/grpc/grpc-options.service.ts
@@ -0,0 +1,31 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { GrpcOptions, Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
+import * as path from 'path';
+import { GRPC_SERVICE } from './service';
+
+@Injectable()
+export class GrpcOptionsService {
+  constructor(private readonly configService: ConfigService) {}
+
+  private readonly logger = new Logger(GrpcOptionsService.name);
+
+  getOptions(): GrpcOptions {
+    this.logger.log(`Initializing gRPC server: ${GRPC_SERVICE.USER}`);
+
+    const port = this.configService.get<string>('GRPC_PORT');
+
+    return {
+      transport: Transport.GRPC,
+      options: {
+        url: `0.0.0.0:${port}`,
+        package: 'user',
+        loader: {
+          keepCase: true,
+        },
+        gracefulShutdown: true,
+        protoPath: path.join(process.cwd(), 'proto/user.proto'),
+      },
+    };
+  }
+}
