de9bbdf19c8a1f8bd8429ec41e681eb22f417eb8
stepan <dark_magic419a@mail.ru>
2025-11-28 09:04:25 +0300

    feature: user-service map-api grpc-implementation

--- user-service/src/infrastructure/adapter/map-api/map-api.adapter-implementation.ts
+++ user-service/src/infrastructure/adapter/map-api/map-api.adapter-implementation.ts
@@ -1,15 +1,30 @@
-import { QueryBus } from '@nestjs/cqrs';
-import { GetCoordsGeocodeQuery } from './query';
-import { Injectable } from '@nestjs/common';
+import {
+  Inject,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
 import { GeocodeView, MapApi } from 'src/application/user/adapter';
+import {
+  GetGeocodeRequest,
+  getGrpcPackageServiceName,
+  GRPC_SERVICE,
+  MapProtoService,
+} from 'src/infrastructure/grpc/service';
+import { ClientGrpc } from '@nestjs/microservices';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable()
 export class MapApiImplementation implements MapApi {
-  constructor(private readonly queryBus: QueryBus) {}
+  private mapGrpcService: MapProtoService;
 
-  async getGeocode(latitude: number, longitude: number): Promise<GeocodeView> {
-    return this.queryBus.execute(
-      new GetCoordsGeocodeQuery({ latitude, longitude }),
+  constructor(@Inject(GRPC_SERVICE.MAP) private readonly client: ClientGrpc) {}
+
+  private readonly logger = new Logger(MapApiImplementation.name);
+
+  onModuleInit() {
+    this.mapGrpcService = this.client.getService<MapProtoService>(
+      getGrpcPackageServiceName(GRPC_SERVICE.MAP),
     );
   }
 }
