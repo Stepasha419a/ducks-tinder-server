0d1fd78357658a204261e346d1dfa742a3dfd2ed
stepan <dark_magic419a@mail.ru>
2025-11-28 14:40:40 +0300

    feature: map-service makefile specify grpc-java-plugin path

---
+++ map-service-ij/src/test/java/ducks_tinder/com/map_service/MapServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ducks_tinder.com.map_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
