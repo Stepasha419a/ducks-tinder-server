6bbe137c18b72f2f205201a55501dc7fbbbf64ec
stepan <dark_magic419a@mail.ru>
2025-11-28 09:07:21 +0300

    feature: user-service map-proto-service get-geocode

--- user-service/src/infrastructure/grpc/service/map.proto-service.ts
+++ user-service/src/infrastructure/grpc/service/map.proto-service.ts
@@ -1,3 +1,15 @@
 import { Observable } from 'rxjs';
 
-export interface MapProtoService {}
+export interface MapProtoService {
+  getGeocode(req: GetGeocodeRequest): Observable<GetGeocodeResponse>;
+}
+
+export interface GetGeocodeRequest {
+  latitude: number;
+  longitude: number;
+}
+
+export interface GetGeocodeResponse {
+  name: string;
+  address: string;
+}
