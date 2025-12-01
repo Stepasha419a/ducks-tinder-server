7ce8848af4329f77e7710c0faeb4d6738bd80a37
stepan <dark_magic419a@mail.ru>
2025-12-01 21:24:50 +0300

    feature: user-service grpc-module re-export

--- user-service/src/infrastructure/adapter/index.ts
+++ user-service/src/infrastructure/adapter/index.ts
@@ -1,1 +1,3 @@
 export { MapApiImplementation } from './map-api';
+export { ChatApiImplementation } from './chat-api';
+export { FileApiImplementation } from './file-api';
--- user-service/src/infrastructure/grpc/index.ts
+++ user-service/src/infrastructure/grpc/index.ts
@@ -1,1 +1,2 @@
 export { GrpcModule } from './grpc.module';
+export { GrpcOptionsService } from './grpc-options.service';
