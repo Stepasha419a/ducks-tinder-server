e8b95e31ca6000d162792e11a64a896f82ddc9b7
stepan <dark_magic419a@mail.ru>
2025-12-01 21:34:53 +0300

    feature: user-service grpc-service clients-constant

--- user-service/src/infrastructure/grpc/service/service.ts
+++ user-service/src/infrastructure/grpc/service/service.ts
@@ -1,6 +1,15 @@
 export enum GRPC_SERVICE {
   FILE = 'GRPC_FILE_SERVICE',
+  MAP = 'GRPC_MAP_SERVICE',
+  CHAT = 'GRPC_CHAT_SERVICE',
+  USER = 'GRPC_USER_SERVICE',
 }
+
+export const GRPC_SERVICE_CLIENTS = [
+  GRPC_SERVICE.FILE,
+  GRPC_SERVICE.CHAT,
+  GRPC_SERVICE.MAP,
+];
 
 const info: Record<GRPC_SERVICE, { package: string; service: string }> = {
   [GRPC_SERVICE.FILE]: {
