701f4400191f68b2041083b30484566b89273c29
stepan <dark_magic419a@mail.ru>
2025-12-04 19:27:17 +0300

    feature: user-service health-controller grpc-clients-health

--- user-service/src/interface/health/health.controller.ts
+++ user-service/src/interface/health/health.controller.ts
@@ -5,6 +5,11 @@ import {
   PrismaHealthIndicator,
 } from '@nestjs/terminus';
 import { Public } from '../common';
+import { RabbitMQHealthIndicator } from '../../infrastructure/health/rabbitmq.health';
+import { DatabaseService } from 'src/infrastructure/database';
+import { GrpcHealthIndicator } from 'src/infrastructure/health/grpc.health';
+import { getGrpcPackageName } from 'src/infrastructure/grpc/service';
+import { GRPC_SERVICE_CLIENTS } from 'src/infrastructure/grpc/service/service';
 
 @Public()
 @Controller('health')
@@ -12,6 +17,7 @@ export class HealthController {
   constructor(
     private readonly health: HealthCheckService,
     private readonly rmqHealth: RabbitMQHealthIndicator,
+    private readonly grpcHealth: GrpcHealthIndicator,
     private readonly db: DatabaseService,
     private readonly prismaHealth: PrismaHealthIndicator,
   ) {}
@@ -30,6 +36,13 @@ export class HealthController {
     return this.health.check([
       () => this.prismaHealth.pingCheck('database', this.db),
       () => this.rmqHealth.isHealthy('rabbitmq'),
+      ...Object.values(GRPC_SERVICE_CLIENTS).map(
+        (service) => () =>
+          this.grpcHealth.isHealthy(
+            `${getGrpcPackageName(service)}ServiceGrpc`,
+            service,
+          ),
+      ),
     ]);
   }
 
