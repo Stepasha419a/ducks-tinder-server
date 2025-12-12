0ec47f6ad87d1269ae13ee1afdc9a3cce2b1aa8f
stepan <dark_magic419a@mail.ru>
2025-12-12 22:13:06 +0300

    feature: subscription-service grpc-client init

--- subscription-service/internal/infrastructure/service/grpc_client/grpc_client.go
+++ subscription-service/internal/infrastructure/service/grpc_client/grpc_client.go
@@ -1,11 +1,15 @@
 package grpc_client_service
 
 import (
+	"context"
+	"crypto/tls"
 	"sync/atomic"
 	"time"
 
 	connection_service "github.com/Stepasha419a/ducks-tinder-server/subscription-service/internal/domain/service/connection"
+	tls_service "github.com/Stepasha419a/ducks-tinder-server/subscription-service/internal/infrastructure/service/tls"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/keepalive"
 )
 
@@ -28,4 +32,17 @@ var kacp = keepalive.ClientParameters{
 	Time:                10 * time.Second,
 	Timeout:             5 * time.Second,
 	PermitWithoutStream: true,
+}
+
+func NewGrpcClientService(ctx context.Context, tlsService *tls_service.TlsService, connectionService *connection_service.ConnectionService, options *GrpcClientServiceOptions) (*GrpcClientService, func()) {
+	tlsConfig := tlsService.GetConfig(options.TlsServerName)
+
+	tlsConfig.ClientAuth = tls.RequireAndVerifyClientCert
+	creds := credentials.NewTLS(tlsConfig)
+
+	opts := []grpc.DialOption{
+		grpc.WithTransportCredentials(creds),
+
+		grpc.WithKeepaliveParams(kacp),
+	}
 }
