97c9da7753457b02de87c8f350829344f8940fa6
stepan <dark_magic419a@mail.ru>
2025-12-14 22:33:04 +0300

    feature: subscription-service health-controller probes

--- subscription-service/internal/interface/http/controller/health/controller.go
+++ subscription-service/internal/interface/http/controller/health/controller.go
@@ -1,7 +1,10 @@
 package health_controller
 
 import (
+	"net/http"
+
 	connection_service "github.com/Stepasha419a/ducks-tinder-server/subscription-service/internal/domain/service/connection"
+	"github.com/gofiber/fiber/v3"
 )
 
 type HealthController struct {
@@ -16,4 +19,17 @@ func NewHealthController(f *fiber.App, connectionService *connection_service.Con
 	f.Get("/startupz", controller.GetProbe)
 
 	return controller
+}
+
+func (hc *HealthController) GetReadyProbe(ctx fiber.Ctx) error {
+	report := hc.connectionService.GetConnectionStateReport()
+	if report.Status == connection_service.StateDown {
+		return ctx.Status(http.StatusServiceUnavailable).JSON(report)
+	}
+
+	return ctx.JSON(report)
+}
+
+func (hc *HealthController) GetProbe(ctx fiber.Ctx) error {
+	return ctx.Status(http.StatusOK).End()
 }
