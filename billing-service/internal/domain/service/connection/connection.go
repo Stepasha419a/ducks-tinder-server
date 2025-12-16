2c3b7bd6bfac9e1b1bf9e28e87aec05b530810ad
stepan <dark_magic419a@mail.ru>
2025-12-16 20:32:24 +0300

    feature: billing-service connection-service get-connection-report

--- billing-service/internal/domain/service/connection/connection.go
+++ billing-service/internal/domain/service/connection/connection.go
@@ -51,3 +51,23 @@ func (m *ConnectionService) UpdateState(t string, healthy bool, err error) {
 
 	m.States[t] = &ConnectionState{Type: t, Healthy: healthy, Error: err, Updated: time.Now()}
 }
+
+func (m *ConnectionService) GetConnectionStateReport() *ConnectionStateReport {
+	report := &ConnectionStateReport{Status: StateUp, Info: make(map[string]*ConnectionStateReportInfo), Error: make(map[string]*ConnectionStateReportError)}
+
+	m.Mu.RLock()
+	defer m.Mu.RUnlock()
+
+	for key, state := range m.States {
+		report.Info[key] = &ConnectionStateReportInfo{Status: StateUp}
+
+		if !state.Healthy {
+			report.Status = StateDown
+
+			report.Info[key].Status = StateDown
+			report.Error[key] = &ConnectionStateReportError{Status: StateDown, Error: state.Error.Error()}
+		}
+	}
+
+	return report
+}
