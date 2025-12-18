c76061824cca8699825ada8feae7ca8e10afa8de
stepan <dark_magic419a@mail.ru>
2025-12-18 21:06:23 +0300

    feature: chat-service grpc-service info constant

--- chat-service/src/infrastructure/grpc/service/service.ts
+++ chat-service/src/infrastructure/grpc/service/service.ts
@@ -6,3 +6,14 @@ export enum GRPC_SERVICE {
 export const GRPC_SERVICE_CLIENTS = [GRPC_SERVICE.USER];
 
 export const CRITICAL_GRPC_SERVICE_CLIENTS = [];
+
+const info: Record<GRPC_SERVICE, { package: string; service: string }> = {
+  [GRPC_SERVICE.CHAT]: {
+    package: 'chat',
+    service: 'ChatService',
+  },
+  [GRPC_SERVICE.USER]: {
+    package: 'user',
+    service: 'UserService',
+  },
+};
