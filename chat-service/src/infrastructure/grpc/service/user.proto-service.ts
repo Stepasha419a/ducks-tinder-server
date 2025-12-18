3936f7d5a36d48e87bd951e46b54a42bdcd9efcd
stepan <dark_magic419a@mail.ru>
2025-12-18 21:05:31 +0300

    feature: chat-service user-service-proto ts-type methods

--- chat-service/src/infrastructure/grpc/service/user.proto-service.ts
+++ chat-service/src/infrastructure/grpc/service/user.proto-service.ts
@@ -1,4 +1,16 @@
 import { Observable } from 'rxjs';
 import { ChatMemberView } from 'src/application/adapter/user-api/view';
 
-export interface UserProtoService {}
+export interface UserProtoService {
+  getShortUser(request: GetShortUserRequest): Observable<ChatMemberView>;
+  createUser(request: CreateUserRequest): Observable<ChatMemberView>;
+}
+
+export interface GetShortUserRequest {
+  id: string;
+}
+
+export class CreateUserRequest {
+  id: string;
+  name: string;
+}
