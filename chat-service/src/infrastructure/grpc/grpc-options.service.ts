28babbcf173735268fe72172e90b471a148af8a3
stepan <dark_magic419a@mail.ru>
2025-12-19 19:22:49 +0300

    feature: chat-service grpc-options-service logger

--- chat-service/src/infrastructure/grpc/grpc-options.service.ts
+++ chat-service/src/infrastructure/grpc/grpc-options.service.ts
@@ -1,13 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { GrpcOptions, Transport } from '@nestjs/microservices';
 import { ConfigService } from '@nestjs/config';
 import * as path from 'path';
+import { GRPC_SERVICE } from './service';
 
 @Injectable()
 export class GrpcOptionsService {
   constructor(private readonly configService: ConfigService) {}
 
+  private readonly logger = new Logger(GrpcOptionsService.name);
+
   getOptions(): GrpcOptions {
+    this.logger.log(`Initializing gRPC server: ${GRPC_SERVICE.CHAT}`);
     const port = this.configService.get<string>('GRPC_PORT');
 
     return {
