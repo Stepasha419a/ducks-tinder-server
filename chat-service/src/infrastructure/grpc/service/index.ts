b686f8c26b3491c5cad706cdc6b1303cff3df1d3
stepan <dark_magic419a@mail.ru>
2025-12-19 19:21:46 +0300

    feature: chat-service grpc-service re-exports from service

---
+++ chat-service/src/infrastructure/grpc/service/index.ts
@@ -0,0 +1,4 @@
+export { GRPC_SERVICE } from './service';
+export { getGrpcPackageServiceName, getGrpcPackageName } from './service';
+export * from './chat.proto-service';
+export * from './user.proto-service';
