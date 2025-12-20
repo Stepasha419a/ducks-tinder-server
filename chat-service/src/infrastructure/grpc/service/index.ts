a227320a77a328854e6dd310ac4ceaa99f53b254
stepan <dark_magic419a@mail.ru>
2025-12-20 20:50:13 +0300

    refactor: chat-service grpc remove proto-services

--- chat-service/src/infrastructure/grpc/service/index.ts
+++
@@ -1,4 +0,0 @@
-export { GRPC_SERVICE } from './service';
-export { getGrpcPackageServiceName, getGrpcPackageName } from './service';
-export * from './chat.proto-service';
-export * from './user.proto-service';
