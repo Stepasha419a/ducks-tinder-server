6a5d63db71a4ec3d9624768faa168114dd3dae01
stepan <dark_magic419a@mail.ru>
2025-12-21 13:15:16 +0300

    feature: chat-service user-api-implementation get-chat-member

--- chat-service/src/infrastructure/adapter/user-api/user-api.adapter-implementation.ts
+++ chat-service/src/infrastructure/adapter/user-api/user-api.adapter-implementation.ts
@@ -1,10 +1,16 @@
-import { Inject, Injectable } from '@nestjs/common';
+import {
+  Inject,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { UserApi } from 'src/application/adapter';
+import { ChatMemberView } from 'src/application/adapter/user-api/view';
 import {
   getGrpcPackageServiceName,
   GRPC_SERVICE,
 } from 'src/infrastructure/grpc/service';
 import { ClientGrpc } from '@nestjs/microservices';
+import { firstValueFrom } from 'rxjs';
 import { user } from 'src/infrastructure/grpc/gen';
 
 @Injectable()
@@ -17,5 +23,13 @@ export class UserApiImplementation implements UserApi {
     this.userGrpcService = this.client.getService<user.UserService>(
       getGrpcPackageServiceName(GRPC_SERVICE.USER),
     );
+  }
+
+  getChatMemberView(memberId: string): Promise<ChatMemberView> {
+    return firstValueFrom<ChatMemberView>(
+      this.userGrpcService.getShortUser({ id: memberId }),
+    ).catch((err) => {
+      throw new InternalServerErrorException();
+    });
   }
 }
