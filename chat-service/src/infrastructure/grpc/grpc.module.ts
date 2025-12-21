e7e47701e3a7c0aead06ee303f9553b9437b99e1
stepan <dark_magic419a@mail.ru>
2025-12-21 13:13:36 +0300

    feature: chat-service grpc-module tls

--- chat-service/src/infrastructure/grpc/grpc.module.ts
+++ chat-service/src/infrastructure/grpc/grpc.module.ts
@@ -3,6 +3,9 @@ import { ClientsModule, Transport } from '@nestjs/microservices';
 import { GRPC_SERVICE_CLIENTS, getGrpcPackageName } from './service';
 import { ConfigService } from '@nestjs/config';
 import { DomainModule } from 'src/domain';
+import { readFileSync } from 'fs';
+import * as path from 'path';
+import { ChannelCredentials } from '@grpc/grpc-js';
 import { GrpcOptionsService } from './grpc-options.service';
 import { UserApi } from 'src/application/adapter';
 import { UserApiImplementation } from '../adapter';
@@ -27,6 +30,19 @@ import { UserApiImplementation } from '../adapter';
 
             logger.log(`Initializing gRPC client: ${name}`);
 
+            const mode = configService.get<string>('NODE_ENV');
+
+            let credentials = ChannelCredentials.createInsecure();
+
+            const rootCertPath = path.join('cert', mode, 'ca.crt');
+            const certPath = path.join('cert', mode, 'tls.crt');
+            const privateKeyPath = path.join('cert', mode, 'tls.key');
+            credentials = ChannelCredentials.createSsl(
+              readFileSync(rootCertPath),
+              readFileSync(privateKeyPath),
+              readFileSync(certPath),
+            );
+
             return {
               transport: Transport.GRPC,
               options: {
@@ -38,6 +54,7 @@ import { UserApiImplementation } from '../adapter';
                 url: configService.get(`${name}_URL`),
                 package: packageName,
                 protoPath: `proto/${packageName}.proto`,
+                credentials,
               },
             };
           },
