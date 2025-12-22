6c2c645a72551a132c7ee9631a41790863ca2f0a
stepan <dark_magic419a@mail.ru>
2025-12-22 20:06:29 +0300

    feature: chat-service grpc-health-indicator

---
+++ chat-service/src/infrastructure/health/grpc.health.ts
@@ -0,0 +1,62 @@
+import { connectivityState } from '@grpc/grpc-js';
+import { Injectable } from '@nestjs/common';
+import { ModuleRef } from '@nestjs/core';
+import { ClientGrpc } from '@nestjs/microservices';
+import {
+  HealthIndicatorResult,
+  HealthIndicatorService,
+} from '@nestjs/terminus';
+import { getGrpcPackageServiceName, GRPC_SERVICE } from '../grpc/service';
+import { Client } from '@grpc/grpc-js';
+
+@Injectable()
+export class GrpcHealthIndicator {
+  constructor(
+    private readonly moduleRef: ModuleRef,
+    private readonly healthIndicatorService: HealthIndicatorService,
+  ) {}
+
+  async isHealthy(
+    key: string,
+    service: GRPC_SERVICE,
+    isCritical = true,
+  ): Promise<HealthIndicatorResult> {
+    const indicator = this.healthIndicatorService.check(key);
+    const serviceName = getGrpcPackageServiceName(service);
+
+    try {
+      const grpcClient = this.moduleRef.get<ClientGrpc>(service, {
+        strict: false,
+      });
+
+      if (!grpcClient) {
+        return indicator.down({
+          message: `Client with token ${service} not found in context`,
+        });
+      }
+
+      const client = grpcClient.getClientByServiceName<Client>(serviceName);
+
+      if (!client) {
+        return indicator.down({
+          message: `${serviceName} gRPC clientInstance not found`,
+        });
+      }
+
+      const channel = client.getChannel();
+      const state = channel.getConnectivityState(true);
+
+      const isHealthy =
+        state === connectivityState.READY || state === connectivityState.IDLE;
+
+      if (isHealthy) {
+        return indicator.up();
+      }
+    } catch (error) {
+      return indicator.down({
+        message: `${serviceName} gRPC check failed`,
+        error: error.message,
+      });
+    }
+  }
+}
