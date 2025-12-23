0efa359316b4e103697debb9b2a6d1cd57675394
stepan <dark_magic419a@mail.ru>
2025-12-23 07:10:29 +0300

    feature: chat-service health-controller

---
+++ chat-service/src/interface/health/health.controller.ts
@@ -0,0 +1,27 @@
+import { Controller, Get } from '@nestjs/common';
+import {
+  HealthCheck,
+  HealthCheckService,
+  PrismaHealthIndicator,
+} from '@nestjs/terminus';
+import { Public } from '../common';
+import { RabbitMQHealthIndicator } from '../../infrastructure/health/rabbitmq.health';
+import { DatabaseService } from 'src/infrastructure/database';
+import { GrpcHealthIndicator } from 'src/infrastructure/health/grpc.health';
+import { getGrpcPackageName } from 'src/infrastructure/grpc/service';
+import {
+  getIsGrpcServiceCritical,
+  GRPC_SERVICE_CLIENTS,
+} from 'src/infrastructure/grpc/service';
+
+@Public()
+@Controller('health')
+export class HealthController {
+  constructor(
+    private readonly health: HealthCheckService,
+    private readonly rmqHealth: RabbitMQHealthIndicator,
+    private readonly grpcHealth: GrpcHealthIndicator,
+    private readonly db: DatabaseService,
+    private readonly prismaHealth: PrismaHealthIndicator,
+  ) {}
+}
