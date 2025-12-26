e97df155a8a4d5abb8df691347807702ceb9fbad
stepan <dark_magic419a@mail.ru>
2025-12-26 21:16:01 +0300

    refactor: user-service grpc relocate service constants

---
+++ user-service/src/infrastructure/grpc/service.ts
@@ -0,0 +1,57 @@
+import { user } from './gen';
+
+export enum GRPC_SERVICE {
+  FILE = 'GRPC_FILE_SERVICE',
+  MAP = 'GRPC_MAP_SERVICE',
+  CHAT = 'GRPC_CHAT_SERVICE',
+  USER = 'GRPC_USER_SERVICE',
+}
+
+export const GRPC_SERVICE_CLIENTS = [
+  GRPC_SERVICE.FILE,
+  GRPC_SERVICE.CHAT,
+  GRPC_SERVICE.MAP,
+];
+
+export const CRITICAL_GRPC_SERVICE_CLIENTS = [
+  GRPC_SERVICE.FILE,
+  GRPC_SERVICE.MAP,
+];
+
+const info: Record<GRPC_SERVICE, { package: string; service: string }> = {
+  [GRPC_SERVICE.FILE]: {
+    package: 'file',
+    service: 'FileService',
+  },
+  [GRPC_SERVICE.MAP]: {
+    package: 'map',
+    service: 'MapService',
+  },
+  [GRPC_SERVICE.CHAT]: {
+    package: 'chat',
+    service: 'ChatService',
+  },
+  [GRPC_SERVICE.USER]: {
+    package: 'user',
+    service: 'UserService',
+  },
+};
+
+type UserMethods = Capitalize<keyof user.UserService & string>;
+
+export const UserGrpcServiceEndpoints: Record<UserMethods, UserMethods> = {
+  CreateUser: 'CreateUser',
+  GetShortUser: 'GetShortUser',
+};
+
+export function getGrpcPackageName(name: GRPC_SERVICE) {
+  return info[name].package;
+}
+
+export function getGrpcPackageServiceName(name: GRPC_SERVICE) {
+  return info[name].service;
+}
+
+export function getIsGrpcServiceCritical(name: GRPC_SERVICE) {
+  return CRITICAL_GRPC_SERVICE_CLIENTS.includes(name);
+}
