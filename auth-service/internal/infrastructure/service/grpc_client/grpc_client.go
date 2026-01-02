acb9784a6a4b45a59e7fc11172deefb546146750
stepan <dark_magic419a@mail.ru>
2026-01-02 16:34:02 +0300

    feature: auth-service grpc-client

---
+++ auth-service/internal/infrastructure/service/grpc_client/grpc_client.go
@@ -0,0 +1,32 @@
+package grpc_client_service
+
+import (
+	"sync/atomic"
+	"time"
+
+	connection_service "auth-service/internal/domain/service/connection"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/keepalive"
+)
+
+type GrpcClientService struct {
+	Conn    *grpc.ClientConn
+	IsReady atomic.Bool
+
+	connectionService *connection_service.ConnectionService
+	options           *GrpcClientServiceOptions
+}
+
+type GrpcClientServiceOptions struct {
+	TargetUrl           string
+	TlsServerName       string
+	DisplayName         string
+	ConnectionStateName string
+}
+
+var kacp = keepalive.ClientParameters{
+	Time:                10 * time.Second,
+	Timeout:             5 * time.Second,
+	PermitWithoutStream: true,
+}
