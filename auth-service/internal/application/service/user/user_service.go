44650404cd0cc2814bfe0140a76fa7581477c49f
stepan <dark_magic419a@mail.ru>
2026-01-03 08:15:53 +0300

    feature: auth-service user-service create-user

--- auth-service/internal/application/service/user/user_service.go
+++ auth-service/internal/application/service/user/user_service.go
@@ -1,6 +1,16 @@
 package user_service
 
+import (
+	"context"
+)
+
 type (
 	UserService interface {
+		CreateUser(ctx context.Context, request *CreateUserRequest) error
+	}
+
+	CreateUserRequest struct {
+		Id   string
+		Name string
 	}
 )
