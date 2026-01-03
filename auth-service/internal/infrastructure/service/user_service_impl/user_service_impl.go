c8c82f82a94cad41f46d047aab5f79decd8913a4
stepan <dark_magic419a@mail.ru>
2026-01-03 08:15:04 +0300

    feature: auth-service user-service-impl init

--- auth-service/internal/infrastructure/service/user_service_impl/user_service_impl.go
+++ auth-service/internal/infrastructure/service/user_service_impl/user_service_impl.go
@@ -1,6 +1,11 @@
 package user_service_impl
 
 import (
+	"context"
+
+	user_service "auth-service/internal/application/service/user"
+	connection_service "auth-service/internal/domain/service/connection"
+	config_service "auth-service/internal/infrastructure/service/config"
 	grpc_client_service "auth-service/internal/infrastructure/service/grpc_client"
 	"auth-service/proto/gen"
 )
@@ -11,4 +16,18 @@ var displayName = "user service"
 type UserServiceImpl struct {
 	client  gen.UserServiceClient
 	service *grpc_client_service.GrpcClientService
+}
+
+func NewUserServiceImpl(ctx context.Context, connectionService *connection_service.ConnectionService) (*UserServiceImpl, func()) {
+	targetUrl := config_service.GetConfig().GrpcUserServiceUrl
+	tlsServerName := config_service.GetConfig().TlsServerName
+
+	clientService, cleanUp := grpc_client_service.NewGrpcClientService(ctx, connectionService, &grpc_client_service.GrpcClientServiceOptions{TargetUrl: targetUrl, TlsServerName: tlsServerName, DisplayName: displayName, ConnectionStateName: connectionServiceName})
+	service := &UserServiceImpl{
+		service: clientService,
+	}
+
+	service.client = gen.NewUserServiceClient(service.service.Conn)
+
+	return service, cleanUp
 }
