3a1e2b239319bc6054e47dccad24b3bb60bb5034
stepan <dark_magic419a@mail.ru>
2026-01-04 19:33:44 +0300

    feature: auth-service gin-impl init

--- auth-service/internal/interface/http/gin/gin.go
+++ auth-service/internal/interface/http/gin/gin.go
@@ -12,3 +12,19 @@ type GinImpl struct {
 	name   string
 	port   int
 }
+
+func NewGin(port int, name string, connectionService *connection_service.ConnectionService) *GinImpl {
+	e := gin.Default()
+
+	e.Use(middleware.Cors)
+
+	tlsConfig := tls_service.GetConfig(&config_service.GetConfig().TlsServerName)
+
+	server := &http.Server{
+		Addr:      "0.0.0.0:" + strconv.Itoa(int(port)),
+		Handler:   e,
+		TLSConfig: tlsConfig,
+	}
+
+	return &GinImpl{Engine: e, server: server, port: port, name: name}
+}
