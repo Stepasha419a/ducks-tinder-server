fd36d43e4ddd00224c760fc09e9de203895960b5
Stepasha419a <Dark_Magic419a@mail.ru>
2026-01-27 19:26:21 +0700

    feature: chat-service db-service specify ssl credentials

--- chat-service/src/infrastructure/database/database.service.ts
+++ chat-service/src/infrastructure/database/database.service.ts
@@ -3,12 +3,24 @@ import { ConfigService } from '@nestjs/config';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { Pool } from 'pg';
 import { PrismaClient } from './prisma/client';
+import * as fs from 'fs';
+import * as path from 'path';
 
 @Injectable()
 export class DatabaseService extends PrismaClient implements OnModuleInit {
   constructor(private readonly configService: ConfigService) {
     const pool = new Pool({
       connectionString: configService.get('DATABASE_URL'),
+      ssl: {
+        rejectUnauthorized: false,
+        pfx: fs.readFileSync(
+          path.resolve(
+            process.cwd(),
+            configService.get('CLIENT_IDENTITY_PATH'),
+          ),
+        ),
+        passphrase: configService.get('CLIENT_IDENTITY_PASSWORD'),
+      },
     });
 
     const adapter = new PrismaPg(pool);
