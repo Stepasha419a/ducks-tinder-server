a07621520e7014bf007e1d98de72340e1f80ae4f
Stepasha419a <Dark_Magic419a@mail.ru>
2026-01-31 22:45:26 +0700

    refactor: billing-service fiber-impl remove redundant middlewares

--- billing-service/internal/interface/http/fiber/fiber_impl.go
+++ billing-service/internal/interface/http/fiber/fiber_impl.go
@@ -59,11 +59,6 @@ type FiberImpl struct {
 		AllowCredentials: true,
 	}))
 	app.Use(helmet.New())
-	app.Use(middleware.AuthMiddleware)
-
-	app.Get(healthcheck.LivenessEndpoint, healthcheck.New())
-	app.Get(healthcheck.ReadinessEndpoint, healthcheck.New())
-	app.Get(healthcheck.StartupEndpoint, healthcheck.New())
 
 	return app, func() {
 		log.Println("close fiber app")
