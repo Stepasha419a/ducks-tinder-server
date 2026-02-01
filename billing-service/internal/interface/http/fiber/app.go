641b6f8119a61e08c4d649a34174e988cabf9858
Stepasha419a <Dark_Magic419a@mail.ru>
2026-02-01 22:40:48 +0700

    feature: billing-service fiber-app http

--- billing-service/internal/interface/http/fiber/app.go
+++ billing-service/internal/interface/http/fiber/app.go
@@ -1,7 +1,29 @@
 package fiber_impl
 
+import (
+	"billing-service/internal/application/service"
+	config_service "billing-service/internal/domain/service/config"
+	connection_service "billing-service/internal/domain/service/connection"
+	validator_service "billing-service/internal/domain/service/validator"
+	billing_controller "billing-service/internal/interface/http/controller/billing"
+	health_controller "billing-service/internal/interface/http/controller/health"
+	metrics_controller "billing-service/internal/interface/http/controller/metrics"
+	"billing-service/internal/interface/http/middleware"
+)
+
 type HttpFiberApp struct {
 	Fiber *FiberImpl
+}
+
+func NewHttpFiberApp(middleware *middleware.Middleware, service service.BillingService, configService config_service.ConfigService, validatorService validator_service.ValidatorService) (*HttpFiberApp, func()) {
+	port := int(configService.GetConfig().Port)
+	httpApp, cleaner := NewFiberApp(port, "http", configService)
+
+	httpApp.App.Use(middleware.AuthMiddleware)
+
+	billing_controller.NewBillingController(httpApp.App, service, validatorService)
+
+	return &HttpFiberApp{Fiber: httpApp}, cleaner
 }
 
 type HealthFiberApp struct {
