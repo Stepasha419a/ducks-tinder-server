13884023df88c8f92b2d3104b50214352a7da050
Stepasha419a <Dark_Magic419a@mail.ru>
2026-02-05 10:42:19 +0700

    feature: file-service request-validation to-upload-file-request

--- file-service/internal/interface/common/request.go
+++ file-service/internal/interface/common/request.go
@@ -1,5 +1,22 @@
 package interface_common
 
+import (
+	"go-file-server/internal/handler"
+	validator_service "go-file-server/internal/service/validator"
+)
+
+func (dto *UploadFileDto) ToUploadFileRequest(validatorService *validator_service.ValidatorService) (*handler.UploadFileRequest, error) {
+	err := validatorService.Struct(dto)
+	if err != nil {
+		return nil, err
+	}
+
+	return &handler.UploadFileRequest{
+		Data: dto.Data,
+		Type: dto.Type,
+	}, nil
+}
+
 type UploadFileDto struct {
 	Data []byte `validate:"required"`
 	Type string `validate:"required"`
