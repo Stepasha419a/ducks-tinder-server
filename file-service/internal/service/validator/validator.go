801c1facf8fc8cac89bdc8cef543a65612be14a7
Stepasha419a <Dark_Magic419a@mail.ru>
2026-02-05 10:37:34 +0700

    feature: file-service validator-service init

--- file-service/internal/service/validator/validator.go
+++ file-service/internal/service/validator/validator.go
@@ -1,9 +1,21 @@
 package validator_service
 
 import (
+	"log"
+
 	"github.com/go-playground/validator/v10"
 )
 
 type ValidatorService struct {
 	validate *validator.Validate
+}
+
+func NewValidatorService() *ValidatorService {
+	log.Println("new validator service")
+
+	validate := validator.New()
+
+	return &ValidatorService{
+		validate,
+	}
 }
