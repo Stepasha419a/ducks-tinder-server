dd8d517da9679545945bc187386087d90fac4fa3
Stepasha419a <Dark_Magic419a@mail.ru>
2026-02-10 18:53:54 +0700

    feature: user-service chat-api adapter-implimentation use new prisma types

--- user-service/src/infrastructure/adapter/chat-api/chat-api.adapter-implementation.ts
+++ user-service/src/infrastructure/adapter/chat-api/chat-api.adapter-implementation.ts
@@ -8,31 +8,31 @@ import { ClientGrpc } from '@nestjs/microservices';
 import { firstValueFrom } from 'rxjs';
 import { ChatApi } from 'src/application/user/adapter';
 import { CreateChatDto } from 'src/application/user/adapter/chat-api/dto';
+import { chat } from 'src/infrastructure/grpc/gen';
 import {
-  ChatProtoService,
-  ChatResponse,
   getGrpcPackageServiceName,
   GRPC_SERVICE,
 } from 'src/infrastructure/grpc/service';
 
 @Injectable()
 export class ChatApiImplementation implements ChatApi {
-  private chatGrpcService: ChatProtoService;
+  private chatGrpcService: chat.ChatService;
 
   constructor(@Inject(GRPC_SERVICE.CHAT) private readonly client: ClientGrpc) {}
 
   private readonly logger = new Logger(ChatApiImplementation.name);
 
   onModuleInit() {
-    this.chatGrpcService = this.client.getService<ChatProtoService>(
+    this.chatGrpcService = this.client.getService<chat.ChatService>(
       getGrpcPackageServiceName(GRPC_SERVICE.CHAT),
     );
   }
 
-  createChat(dto: CreateChatDto): Promise<ChatResponse> {
+  createChat(dto: CreateChatDto): Promise<chat.ChatResponse> {
     return firstValueFrom(this.chatGrpcService.createChat(dto)).catch((err) => {
       this.logger.error(
         'Failed to handle grpc request',
+        dto,
         err.message,
         err.stack,
       );
