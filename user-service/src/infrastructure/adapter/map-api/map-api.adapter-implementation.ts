05661ce10ed138499e5878c060a829a4a8af41bf
Stepasha419a <Dark_Magic419a@mail.ru>
2026-02-10 18:54:10 +0700

    feature: user-service map-api adapter-implimentation use new prisma types

--- user-service/src/infrastructure/adapter/map-api/map-api.adapter-implementation.ts
+++ user-service/src/infrastructure/adapter/map-api/map-api.adapter-implementation.ts
@@ -6,30 +6,29 @@ import {
 } from '@nestjs/common';
 import { GeocodeView, MapApi } from 'src/application/user/adapter';
 import {
-  GetGeocodeRequest,
   getGrpcPackageServiceName,
   GRPC_SERVICE,
-  MapProtoService,
 } from 'src/infrastructure/grpc/service';
 import { ClientGrpc } from '@nestjs/microservices';
 import { firstValueFrom } from 'rxjs';
+import { map } from 'src/infrastructure/grpc/gen';
 
 @Injectable()
 export class MapApiImplementation implements MapApi {
-  private mapGrpcService: MapProtoService;
+  private mapGrpcService: map.MapService;
 
   constructor(@Inject(GRPC_SERVICE.MAP) private readonly client: ClientGrpc) {}
 
   private readonly logger = new Logger(MapApiImplementation.name);
 
   onModuleInit() {
-    this.mapGrpcService = this.client.getService<MapProtoService>(
+    this.mapGrpcService = this.client.getService<map.MapService>(
       getGrpcPackageServiceName(GRPC_SERVICE.MAP),
     );
   }
 
   getGeocode(latitude: number, longitude: number): Promise<GeocodeView> {
-    const req: GetGeocodeRequest = { latitude, longitude };
+    const req: map.GetGeocodeRequest = { latitude, longitude };
 
     return firstValueFrom(this.mapGrpcService.getGeocode(req)).catch((err) => {
       this.logger.error(
