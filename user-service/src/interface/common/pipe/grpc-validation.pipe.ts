3b4b21d3cfad9f9559da475c84de663e77ea91a8
Stepasha419a <Dark_Magic419a@mail.ru>
2026-02-12 12:14:05 +0700

    feature: user-service grpc-validation-pipe exception-factory

--- user-service/src/interface/common/pipe/grpc-validation.pipe.ts
+++ user-service/src/interface/common/pipe/grpc-validation.pipe.ts
@@ -6,4 +6,17 @@ import { ValidationError } from 'class-validator';
 export const GrpcValidationPipe = new ValidationPipe({
   transform: true,
   whitelist: true,
+  exceptionFactory: (errors: ValidationError[]) => {
+    const messages = errors.map((error) => {
+      const constraints = Object.values(error.constraints || {});
+      return `${error.property}: ${constraints.join(', ')}`;
+    });
+
+    const errorMessage = `Validation failed: ${messages.join('; ')}`;
+
+    return new RpcException({
+      code: status.INVALID_ARGUMENT,
+      message: errorMessage,
+    });
+  },
 });
