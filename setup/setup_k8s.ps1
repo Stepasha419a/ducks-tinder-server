0b43118b992fc52149c14414e941cc2d1782e802
Stepasha419a <Dark_Magic419a@mail.ru>
2026-03-10 23:48:31 +0700

    feature: setup k8s build docker services

--- setup/setup_k8s.ps1
+++ setup/setup_k8s.ps1
@@ -1,3 +1,11 @@
 $services = @('user-service', 'chat-service', 'file-service', 'auth-service', 'billing-service', 'subscription-service', 'map-service')
 
 docker-compose up --build -d $services
+
+foreach ($service in $services) {
+    $sourceImage = "ducks-tinder-$service"
+    $targetImage = "localhost:4999/ducks-tinder-$service"
+    docker tag $sourceImage $targetImage
+
+    docker push $targetImage
+}
