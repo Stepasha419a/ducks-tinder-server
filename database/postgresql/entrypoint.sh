4a8ffbcb9f810ed7a41e0ecb427fbf0429de6955
stepan <dark_magic419a@mail.ru>
2026-03-26 21:41:28 +0300

    feature: postgres entrypoint execute

--- database/postgresql/entrypoint.sh
+++ database/postgresql/entrypoint.sh
@@ -8,3 +8,5 @@ chmod 600 /var/lib/postgresql/tls.key
 
 chown postgres /var/lib/postgresql/ca.crt
 chmod 600 /var/lib/postgresql/ca.crt
+
+exec /usr/local/bin/docker-entrypoint.sh postgres -c config_file=/var/lib/postgresql/postgresql.conf
