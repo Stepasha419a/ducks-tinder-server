3c5a31ad1d310ee5508ffea5cbb5532044248538
stepan <dark_magic419a@mail.ru>
2026-07-11 10:11:11 +0300

    feature: file-service logging-middleware print response

--- file-service/internal/interface/http/middleware.go
+++ file-service/internal/interface/http/middleware.go
@@ -1,7 +1,9 @@
 package http_service
 
 import (
+	"fmt"
 	"net/http"
+	"time"
 )
 
 type responseWriter struct {
@@ -16,11 +18,24 @@ func (rw *responseWriter) WriteHeader(code int) {
 
 func loggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
 		wrappedWriter := &responseWriter{
 			ResponseWriter: w,
 			statusCode:     http.StatusOK,
 		}
 
 		next.ServeHTTP(wrappedWriter, r)
+
+		duration := time.Since(start)
+
+		fmt.Printf("[MUX] %s | %3d | %13v | %15s | %-7s %s\n",
+			time.Now().Format("2006/01/02 - 15:04:05"),
+			wrappedWriter.statusCode,
+			duration,
+			r.RemoteAddr,
+			r.Method,
+			r.URL.Path,
+		)
 	})
 }
