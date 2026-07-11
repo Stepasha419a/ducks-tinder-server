61a51a67f072f64968837b8cd21cf908529ae497
stepan <dark_magic419a@mail.ru>
2026-07-11 10:13:58 +0300

    refactor: database-seeder remove billing-service seeder call

--- database-seeder/internal/service/seeder/seeder.go
+++ database-seeder/internal/service/seeder/seeder.go
@@ -3,7 +3,6 @@ package seeder_service
 import (
 	database_service "database-seeder/internal/service/database"
 	auth_service_seeder "database-seeder/internal/service/seeder/auth_service"
-	billing_service_seeder "database-seeder/internal/service/seeder/billing_service"
 	map_service_seeder "database-seeder/internal/service/seeder/map_service"
 	prisma_seeder "database-seeder/internal/service/seeder/prisma"
 	"log"
@@ -13,12 +12,6 @@ func Seed(databaseInstances *database_service.DatabaseInstances) {
 	err := auth_service_seeder.SeedAuthServicePostgres(databaseInstances.AuthServicePostgresInstance)
 	if err != nil {
 		log.Print("seed auth service postgres - error")
-		panic(err)
-	}
-
-	err = billing_service_seeder.SeedBillingServicePostgres(databaseInstances.BillingServicePostgresInstance)
-	if err != nil {
-		log.Print("seed billing service postgres - error")
 		panic(err)
 	}
 
