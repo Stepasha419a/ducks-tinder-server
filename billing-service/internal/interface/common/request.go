75fe71b559c582d512ed3880d0dca6b53fa88a6f
stepan <dark_magic419a@mail.ru>
2026-07-18 11:07:05 +0300

    refactor: billing-service request remove credit-card methods

--- billing-service/internal/interface/common/request.go
+++ billing-service/internal/interface/common/request.go
@@ -1,83 +1,23 @@
 package interface_common
 
 import (
-	"billing-service/internal/application/command/add_user_credit_card"
-	"billing-service/internal/application/command/delete_user_credit_card"
-	"billing-service/internal/application/command/withdraw_user_credit_card"
-	get_user_credit_card "billing-service/internal/application/query/get-user-credit-card"
+	"billing-service/internal/application/command/handle_user_purchase"
 	validator_service "billing-service/internal/domain/service/validator"
 )
 
-type GetUserCreditCardDto struct {
+type HandleUserPurchaseDto struct {
 	UserId string `validate:"required,uuid4"`
+	Amount int64  `validate:"required,number,min=1"`
 }
 
-func (dto *GetUserCreditCardDto) ToGetUserCreditCardQuery(validatorService validator_service.ValidatorService) (*get_user_credit_card.GetUserCreditCardQuery, error) {
+func (dto *HandleUserPurchaseDto) ToHandleUserPurchaseCommand(validatorService validator_service.ValidatorService) (*handle_user_purchase.HandleUserPurchaseCommand, error) {
 	err := validatorService.Struct(dto)
 	if err != nil {
 		return nil, err
 	}
 
-	return &get_user_credit_card.GetUserCreditCardQuery{
+	return &handle_user_purchase.HandleUserPurchaseCommand{
 		UserId: dto.UserId,
-	}, nil
-}
-
-type AddUserCreditCardDto struct {
-	UserId    string `validate:"required,uuid4"`
-	Pan       string `validate:"credit_card"`
-	Holder    string `validate:"credit_card_holder"`
-	Cvc       string `validate:"credit_card_cvc"`
-	ExpiresAt string `validate:"credit_card_expires_date_month"`
-}
-
-func (dto *AddUserCreditCardDto) ToAddUserCreditCardCommand(validatorService validator_service.ValidatorService) (*add_user_credit_card.AddUserCreditCardCommand, error) {
-	err := validatorService.Struct(dto)
-	if err != nil {
-		return nil, err
-	}
-
-	return &add_user_credit_card.AddUserCreditCardCommand{
-		UserId:    dto.UserId,
-		Pan:       dto.Pan,
-		Holder:    dto.Holder,
-		Cvc:       dto.Cvc,
-		ExpiresAt: dto.ExpiresAt,
-	}, nil
-}
-
-type DeleteUserCreditCardDto struct {
-	UserId       string `validate:"required,uuid4"`
-	CreditCardId string `validate:"required,uuid4"`
-}
-
-func (dto *DeleteUserCreditCardDto) ToDeleteUserCreditCardCommand(validatorService validator_service.ValidatorService) (*delete_user_credit_card.DeleteUserCreditCardCommand, error) {
-	err := validatorService.Struct(dto)
-	if err != nil {
-		return nil, err
-	}
-
-	return &delete_user_credit_card.DeleteUserCreditCardCommand{
-		UserId:       dto.UserId,
-		CreditCardId: dto.CreditCardId,
-	}, nil
-}
-
-type WithdrawUserCreditCardDto struct {
-	UserId       string `validate:"required,uuid4"`
-	CreditCardId string `validate:"required,uuid4"`
-	Amount       int64  `validate:"required,number,min=1"`
-}
-
-func (dto *WithdrawUserCreditCardDto) ToWithdrawUserCreditCardCommand(validatorService validator_service.ValidatorService) (*withdraw_user_credit_card.WithdrawUserCreditCardCommand, error) {
-	err := validatorService.Struct(dto)
-	if err != nil {
-		return nil, err
-	}
-
-	return &withdraw_user_credit_card.WithdrawUserCreditCardCommand{
-		UserId:       dto.UserId,
-		CreditCardId: dto.CreditCardId,
-		Amount:       dto.Amount,
+		Amount: dto.Amount,
 	}, nil
 }
