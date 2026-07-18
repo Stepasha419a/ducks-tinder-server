e6acb4f17c5cf1864b7a988447ba739ab2de2f0a
stepan <dark_magic419a@mail.ru>
2026-07-18 11:06:44 +0300

    refactor: billing-service validator remove credit-card validation

--- billing-service/internal/infrastructure/service/validator_impl/validator_impl.go
+++ billing-service/internal/infrastructure/service/validator_impl/validator_impl.go
@@ -1,12 +1,8 @@
 package validator_service_impl
 
 import (
-	time_service "billing-service/internal/domain/service/time"
 	validator_service "billing-service/internal/domain/service/validator"
 	"log"
-	"strconv"
-	"strings"
-	"time"
 
 	"github.com/go-playground/validator/v10"
 )
@@ -22,10 +18,6 @@ func NewValidatorService() *ValidatorServiceImpl {
 
 	validate := validator.New()
 
-	validate.RegisterValidation("credit_card_holder", ValidateCreditCardHolder)
-	validate.RegisterValidation("credit_card_cvc", ValidateCreditCardCVC)
-	validate.RegisterValidation("credit_card_expires_date_month", ValidateNotExpiredDateMonth)
-
 	return &ValidatorServiceImpl{
 		validate,
 	}
@@ -33,36 +25,4 @@ func NewValidatorService() *ValidatorServiceImpl {
 
 func (s *ValidatorServiceImpl) Struct(value interface{}) error {
 	return s.validate.Struct(value)
-}
-
-func ValidateCreditCardHolder(fl validator.FieldLevel) bool {
-	value := fl.Field().String()
-
-	parts := strings.Split(value, " ")
-	return len(parts) == 2
-}
-
-func ValidateCreditCardCVC(fl validator.FieldLevel) bool {
-	value := fl.Field().String()
-
-	_, err := strconv.Atoi(value)
-	if err != nil {
-		return false
-	}
-
-	return len(value) == 3 || len(value) == 4
-}
-
-func ValidateNotExpiredDateMonth(fl validator.FieldLevel) bool {
-	value := fl.Field().String()
-	parsedTime, err := time_service.ParseDateMonthString(value)
-	if err != nil {
-		return false
-	}
-
-	if parsedTime.Before(time.Now()) {
-		return false
-	}
-
-	return true
 }
