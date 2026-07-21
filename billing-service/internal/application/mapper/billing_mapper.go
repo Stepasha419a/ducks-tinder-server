843ef6be3da41b2de6712f097c6be7012a3605e8
stepan <dark_magic419a@mail.ru>
2026-07-21 10:17:20 +0300

    refactor: billing-service billing-mapper remove credit-card response

--- billing-service/internal/application/mapper/billing_mapper.go
+++ billing-service/internal/application/mapper/billing_mapper.go
@@ -5,46 +5,19 @@ import (
 	"time"
 )
 
-type CreditCardResponse struct {
-	Id        string    `json:"id"`
-	UserId    string    `json:"userId"`
-	Pan       string    `json:"pan"`
-	Holder    string    `json:"holder"`
-	Cvc       string    `json:"cvc"`
-	ExpiresAt time.Time `json:"expiresAt"`
-
-	CreatedAt time.Time `json:"createdAt"`
-	UpdatedAt time.Time `json:"updatedAt"`
-}
-
-func NewCreditCardResponse(creditCard *entity.CreditCard) *CreditCardResponse {
-	return &CreditCardResponse{
-		Id:        creditCard.Id,
-		UserId:    creditCard.UserId,
-		Pan:       creditCard.Pan,
-		Holder:    creditCard.Holder,
-		Cvc:       creditCard.Cvc,
-		ExpiresAt: creditCard.ExpiresAt,
-		CreatedAt: creditCard.CreatedAt,
-		UpdatedAt: creditCard.UpdatedAt,
-	}
-}
-
 type PurchaseResponse struct {
-	Id           string `json:"id"`
-	UserId       string `json:"userId"`
-	CreditCardId string `json:"creditCardId"`
-	Amount       int64  `json:"amount"`
+	Id     string `json:"id"`
+	UserId string `json:"userId"`
+	Amount int64  `json:"amount"`
 
 	CreatedAt time.Time `json:"createdAt"`
 }
 
 func NewPurchaseResponse(purchase *entity.Purchase) *PurchaseResponse {
 	return &PurchaseResponse{
-		Id:           purchase.Id,
-		UserId:       purchase.UserId,
-		CreditCardId: purchase.CreditCardId,
-		Amount:       purchase.Amount,
+		Id:     purchase.Id,
+		UserId: purchase.UserId,
+		Amount: purchase.Amount,
 
 		CreatedAt: purchase.CreatedAt,
 	}
